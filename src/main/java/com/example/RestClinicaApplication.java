a0d21ef6eb2ac44b9523821fc9c5a3df70c324e6
Arcelio <pc@DESKTOP-AIL5SE4>
2022-08-24 01:59:46 -0500

    Inicio de Proyecto

---
+++ src/main/java/com/example/RestClinicaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClinicaApplication.class, args);
+	}
+
+}
