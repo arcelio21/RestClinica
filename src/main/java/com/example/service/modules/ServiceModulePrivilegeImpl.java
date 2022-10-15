a7603ab59d39ebfb7cd2cc6afb48fe001144b65a
arcelioM <arceliomonte15@gmail.com>
2022-10-14 21:48:37 -0500

    Operaciones de servicios basicas creadas para 'tModulePrivilege'

---
+++ src/main/java/com/example/service/modules/ServiceModulePrivilegeImpl.java
@@ -0,0 +1,58 @@
+package com.example.service.modules;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.example.entity.modules.TmodulePrivilege;
+import com.example.mapper.modules.MapperModulePrivilege;
+
+public class ServiceModulePrivilegeImpl implements IServiceModulePrivilege{
+	
+	private MapperModulePrivilege mapperModulePrivilege;
+	
+	
+
+	public ServiceModulePrivilegeImpl(MapperModulePrivilege mapperModulePrivilege) {
+		this.mapperModulePrivilege = mapperModulePrivilege;
+	}
+
+	@Override
+	public List<TmodulePrivilege> getAll() {
+
+		List<TmodulePrivilege> modulesPriv=this.mapperModulePrivilege.getAll();
+		if(modulesPriv==null || modulesPriv.isEmpty()) {
+			return Collections.emptyList();
+		}
+		
+		return modulesPriv;
+	}
+
+	@Override
+	public TmodulePrivilege getById(Integer id) {
+
+		if(id==null || id<=0) {
+			return null;
+		}
+		
+		return this.mapperModulePrivilege.getById(id);
+	}
+
+	@Override
+	public Integer update(TmodulePrivilege tmodulePrivilege) {
+
+		if(tmodulePrivilege==null || tmodulePrivilege.getId()==null || tmodulePrivilege.getId()<=0) {
+			return 0;
+		}
+		return this.mapperModulePrivilege.update(tmodulePrivilege);
+	}
+
+	@Override
+	public Integer save(TmodulePrivilege tmodulePrivilege) {
+
+		if(tmodulePrivilege==null || tmodulePrivilege.getId()<=0) {
+			return 0;
+		}
+		return this.mapperModulePrivilege.save(tmodulePrivilege);
+	}
+
+}
