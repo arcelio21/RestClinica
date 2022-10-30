9cfaad73516d5f394d5dc4abe14fcd8fef33758c
arcelioM <arceliomonte15@gmail.com>
2022-10-30 00:10:46 -0500

    Interface de servicio para TuserTypeReg

---
+++ src/main/java/com/example/service/user/IServiceUserTypeReg.java
@@ -0,0 +1,12 @@
+ package com.example.service.user;
+
+import com.example.service.ServiceTemplateCrud;
+
+public interface IServiceUserTypeReg<T,I> extends ServiceTemplateCrud<T,I>{
+
+  
+	
+}
+
+
+ 
