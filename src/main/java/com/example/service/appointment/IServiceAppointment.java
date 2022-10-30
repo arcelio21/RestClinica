2e674d4802d232e97dabbe87872609fc37a87636
arcelioM <arceliomonte15@gmail.com>
2022-10-30 12:30:47 -0600

    Funcionalidades exclusicas para Appointment agregada a Interface de servicio

--- src/main/java/com/example/service/appointment/IServiceAppointment.java
+++ src/main/java/com/example/service/appointment/IServiceAppointment.java
@@ -1,8 +1,31 @@
 package com.example.service.appointment;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.example.entity.appointment.Tappointment;
+import com.example.entity.speciality.Tspeciality;
+import com.example.entity.speciality.TuserSpeciality;
+import com.example.entity.status.Tstatus;
+import com.example.entity.user.TuserTypeReg;
 import com.example.service.ServiceTemplateCrud;
 
-public interface IServiceAppointment<T,I> extends ServiceTemplateCrud<T,I>{
+public interface IServiceAppointment<T, I> extends ServiceTemplateCrud<T, I> {
 
-	
+	List<Tappointment> getByPatientId(TuserTypeReg tuserTypeReg, Tstatus tstatus);
+
+	List<Tappointment> getBydSpecialityId(Tspeciality tspeciality, Tstatus tstatus);
+
+	List<Tappointment> getBySecretaryId(TuserTypeReg tuserTypeReg, Tstatus tstatus);
+
+	List<Tappointment> getBySpecialityPhysicianId(TuserSpeciality tuserSpeciality, Tstatus tstatus);
+
+	List<Tappointment> getByCreationDate(LocalDateTime dateTime, Tstatus tstatus);
+
+	List<Tappointment> getByVisitDate(LocalDateTime dateTime, Tstatus tstatus);
+
+	List<Tappointment> getByStatusId(Tstatus tstatus);
+
+	Integer changeStatus(Tappointment tappointment);
+
 }
