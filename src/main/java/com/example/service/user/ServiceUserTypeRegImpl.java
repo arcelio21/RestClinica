2b6e50ec71080593a52c599e2a585cd633ff6bea
arcelioM <arceliomonte15@gmail.com>
2022-10-30 00:11:34 -0500

    Implementacion para servicio de TuserTypeReg creado, y funcionalidad de obtencion de todos los registros agregadps

---
+++ src/main/java/com/example/service/user/ServiceUserTypeRegImpl.java
@@ -0,0 +1,45 @@
+package com.example.service.user;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.example.entity.user.TuserTypeReg;
+import com.example.mapper.user.MapperUserTypeReg;
+
+public class ServiceUserTypeRegImpl implements IServiceUserTypeReg<TuserTypeReg, Integer> {
+
+	private MapperUserTypeReg mapperUserTypeReg;
+
+	public ServiceUserTypeRegImpl(MapperUserTypeReg mapperUserTypeReg) {
+		this.mapperUserTypeReg = mapperUserTypeReg;
+	}
+
+	@Override
+	public List<TuserTypeReg> getAll() {
+		// TODO Auto-generated method stub
+		List<TuserTypeReg> tuserTypeRegs= this.mapperUserTypeReg.getAll();
+		if(tuserTypeRegs.size()<=0){
+		  return Collections.emptyList();
+		}
+		return tuserTypeRegs;
+	}
+
+	@Override
+	public TuserTypeReg getById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer update(TuserTypeReg t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer save(TuserTypeReg t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
