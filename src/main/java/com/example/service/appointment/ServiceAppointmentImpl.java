cf2c49e0f906f9a9535d0db5d3eac2b152554ff3
arcelioM <arceliomonte15@gmail.com>
2022-10-30 13:37:00 -0600

    Funcionalidades CRUD extras para servicio de Appointment implementado

--- src/main/java/com/example/service/appointment/ServiceAppointmentImpl.java
+++ src/main/java/com/example/service/appointment/ServiceAppointmentImpl.java
@@ -1,9 +1,14 @@
 package com.example.service.appointment;
 
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 
 import com.example.entity.appointment.Tappointment;
+import com.example.entity.speciality.Tspeciality;
+import com.example.entity.speciality.TuserSpeciality;
+import com.example.entity.status.Tstatus;
+import com.example.entity.user.TuserTypeReg;
 import com.example.mapper.appointment.MapperAppointment;
 
 public class ServiceAppointmentImpl implements IServiceAppointment<Tappointment, Integer> {
@@ -16,10 +21,10 @@ public class ServiceAppointmentImpl implements IServiceAppointment<Tappointment,
 
 	@Override
 	public List<Tappointment> getAll() {
-	
+
 		List<Tappointment> aTappointments = this.mapperAppointment.getAll();
 
-		if(aTappointments==null || aTappointments.size()<=0){
+		if (aTappointments == null || aTappointments.size() <= 0) {
 			return Collections.emptyList();
 		}
 		return aTappointments;
@@ -28,7 +33,7 @@ public class ServiceAppointmentImpl implements IServiceAppointment<Tappointment,
 	@Override
 	public Tappointment getById(Integer id) {
 
-		if(id==null || id<=0){
+		if (id == null || id <= 0) {
 			return null;
 		}
 		return this.mapperAppointment.getById(id);
@@ -37,7 +42,7 @@ public class ServiceAppointmentImpl implements IServiceAppointment<Tappointment,
 	@Override
 	public Integer update(Tappointment tappointment) {
 
-		if(tappointment==null || tappointment.getId()==null || tappointment.getId()<=0){
+		if (tappointment == null || tappointment.getId() == null || tappointment.getId() <= 0) {
 			return 0;
 		}
 		return this.mapperAppointment.update(tappointment);
@@ -46,10 +51,89 @@ public class ServiceAppointmentImpl implements IServiceAppointment<Tappointment,
 	@Override
 	public Integer save(Tappointment tappointment) {
 
-		if(tappointment==null || tappointment.getId()!=null){
+		if (tappointment == null || tappointment.getId() != null) {
 			return 0;
 		}
 		return this.mapperAppointment.save(tappointment);
+	}
+
+	@Override
+	public List<Tappointment> getByPatientId(TuserTypeReg tuserTypeReg, Tstatus tstatus) {
+
+		if (tuserTypeReg == null || tuserTypeReg.getId() == null || tuserTypeReg.getId() <= 0 || tstatus == null
+				|| tstatus.getId() == null) {
+			return Collections.emptyList();
+		}
+
+		return this.mapperAppointment.getByPatientId(tuserTypeReg, tstatus);
+	}
+
+	@Override
+	public List<Tappointment> getBydSpecialityId(Tspeciality tspeciality, Tstatus tstatus) {
+
+		if (tspeciality == null || tspeciality.getId() == null || tspeciality.getId() <= 0 || tstatus == null
+				|| tstatus.getId() == null) {
+			return Collections.emptyList();
+		}
+		return this.mapperAppointment.getBydSpecialityId(tspeciality, tstatus);
+	}
+
+	@Override
+	public List<Tappointment> getBySecretaryId(TuserTypeReg tuserTypeReg, Tstatus tstatus) {
+
+		if (tuserTypeReg == null || tuserTypeReg.getId() == null || tuserTypeReg.getId() <= 0 || tstatus == null
+				|| tstatus.getId() == null) {
+			return Collections.emptyList();
+		}
+		return this.mapperAppointment.getBySecretaryId(tuserTypeReg, tstatus);
+	}
+
+	@Override
+	public List<Tappointment> getBySpecialityPhysicianId(TuserSpeciality tuserSpeciality, Tstatus tstatus) {
+
+		if (tuserSpeciality == null || tuserSpeciality.getId() == null || tuserSpeciality.getId() <= 0
+				|| tstatus == null || tstatus.getId() == null) {
+			return Collections.emptyList();
+		}
+		return this.mapperAppointment.getBySpecialityPhysicianId(tuserSpeciality, tstatus);
+	}
+
+	@Override
+	public List<Tappointment> getByCreationDate(LocalDateTime dateTime, Tstatus tstatus) {
+
+		if (dateTime == null || tstatus == null || tstatus.getId() == null) {
+			return Collections.emptyList();
+		}
+		return this.mapperAppointment.getByCreationDate(dateTime, tstatus);
+	}
+
+	@Override
+	public List<Tappointment> getByVisitDate(LocalDateTime dateTime, Tstatus tstatus) {
+
+		if (dateTime == null || tstatus == null || tstatus.getId() == null) {
+			return Collections.emptyList();
+		}
+
+		return this.mapperAppointment.getByVisitDate(dateTime, tstatus);
+	}
+
+	@Override
+	public List<Tappointment> getByStatusId(Tstatus tstatus) {
+
+		if (tstatus == null || tstatus.getId() == null) {
+			return Collections.emptyList();
+		}
+		return this.mapperAppointment.getByStatusId(tstatus);
+	}
+
+	@Override
+	public Integer changeStatus(Tappointment tappointment) {
+
+		if (tappointment == null || tappointment.getId() == null || tappointment.getId() <= 0
+				|| tappointment.getStatusId() == null || tappointment.getStatusId().getId() == null) {
+			return 0;
+		}
+		return this.mapperAppointment.changeStatus(tappointment);
 	}
 
 }
