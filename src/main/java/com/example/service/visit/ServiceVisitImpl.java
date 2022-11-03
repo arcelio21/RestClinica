98c91b6611d0dd88905054249108aa6e00418a15
arcelioM <arceliomonte15@gmail.com>
2022-11-03 12:22:08 -0500

    Funcionalidad para busqueda de visit por ID  de especialidad agregada

--- src/main/java/com/example/service/visit/ServiceVisitImpl.java
+++ src/main/java/com/example/service/visit/ServiceVisitImpl.java
@@ -68,8 +68,8 @@ public class ServiceVisitImpl implements IServiceVisit<Tvisit, Integer> {
 	}
 
 	/**
-	 * @param reg : Usuario de tipo paciente con su ID
-	 * @param tstatus      : Status de las consulta{Tvisit} que se esta buscando
+	 * @param reg     : Usuario de tipo paciente con su ID
+	 * @param tstatus : Status de las consulta{Tvisit} que se esta buscando
 	 * @return tvisits: retornara una lista con las consultas encontradas, en
 	 *         caso de no haber devolvera una lista vacia}
 	 */
@@ -95,13 +95,13 @@ public class ServiceVisitImpl implements IServiceVisit<Tvisit, Integer> {
 	 * @apiNote Este metodo se encargara de buscar los registros de consultas
 	 *          filtradas por la especialidad de una enfermera y el estado de las
 	 *          consultas que busca
-	 * @param 	tuserSpeciality 
-	 *                          especialidad de la enfermera por la que filtrara la
+	 * @param tuserSpeciality
+	 *                        especialidad de la enfermera por la que filtrara la
 	 *
-	 *                          consulta con su ID
+	 *                        consulta con su ID
 	 * @param tstatus
-	 *                          Status de las consulta{Tvisit} que se esta buscando
-	 * @return tvisits 
+	 *                        Status de las consulta{Tvisit} que se esta buscando
+	 * @return tvisits
 	 *         Lista de consultas encontradas, en caso de no
 	 *         encontrar registro devolvera una lista vacia
 	 */
@@ -126,8 +126,8 @@ public class ServiceVisitImpl implements IServiceVisit<Tvisit, Integer> {
 	 * @apiNote Este metodo se encargar de buscar las consultas disponibles filtrado
 	 *          por el valor de los parametros enviados al metodo
 	 * @param speciality : Especialidad del medico por el que se filtrara las
-	 *                        consultas
-	 * @param  tstatus  : Tipo de estado de las consultas que se desean buscar
+	 *                   consultas
+	 * @param tstatus    : Tipo de estado de las consultas que se desean buscar
 	 * @return tvisits : Devolvera una lista de consultas
 	 *
 	 */
@@ -136,29 +136,41 @@ public class ServiceVisitImpl implements IServiceVisit<Tvisit, Integer> {
 	public List<Tvisit> getByPhysicianId(TuserSpeciality speciality, Tstatus tstatus) {
 
 		if (speciality == null || speciality.getId() == null || speciality.getId() <= 0 || tstatus == null
-		|| tstatus.getId() == null || tstatus.getId() <= 0){
+				|| tstatus.getId() == null || tstatus.getId() <= 0) {
 			return Collections.emptyList();
 		}
 
 		List<Tvisit> tvisits = this.mapperVisit.getByPhysicianId(speciality, tstatus);
 
-		if(tvisits==null || tvisits.size()<=0){
+		if (tvisits == null || tvisits.size() <= 0) {
 			return Collections.emptyList();
 		}
 		return tvisits;
 	}
 
 	/**
-	 *@apiNote Metodo que buscara las consultas disponible filtrada por espcialidad y estado de acuerdo a los valores enviado por parametro
-	 *@param tspeciality : Especialidad 
-	 *@param tstatus : Estado de las consultas
-	 *@return tvisits : Listado de las consultas encontradas
-	 * */
+	 * @apiNote Metodo que buscara las consultas disponible filtrada por espcialidad
+	 *          y estado de acuerdo a los valores enviado por parametro
+	 * @param tspeciality : Especialidad
+	 * @param tstatus     : Estado de las consultas
+	 * @return tvisits : Listado de las consultas encontradas
+	 */
 	@Override
 	@Transactional(readOnly = true)
 	public List<Tvisit> getBySpecialityId(Tspeciality tspeciality, Tstatus tstatus) {
 
-		return null;
+		if (tspeciality == null || tspeciality.getId() == null || tspeciality.getId() <= 0 || tstatus == null
+		|| tstatus.getId() == null || tstatus.getId() <= 0){
+			return Collections.emptyList();
+		}
+
+		List<Tvisit> tvisits= this.mapperVisit.getBySpecialityId(tspeciality, tstatus);
+
+		if(tvisits==null || tvisits.size()<=0){
+			return Collections.emptyList();
+		}
+
+		return tvisits;
 	}
 
 	@Override
