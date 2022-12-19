90ccb52507a6571ba911b67f9a5e1fa06fdea744
Arcelio Montezuma <arceliomonte15@gmail.com>
2022-12-19 17:25:10 -0500

    Controlador de provincia modificado

--- src/main/java/com/example/controller/address/ControllerProvinceImpl.java
+++ src/main/java/com/example/controller/address/ControllerProvinceImpl.java
@@ -3,10 +3,13 @@ package com.example.controller.address;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import com.example.controller.ControllerTemplateImpl;
 import com.example.entity.address.Tprovince;
 import com.example.service.address.ServiceProvinceImpl;
 
+@Tag(name = "Provinces Controller", description = "Realiza operaciones sobre las provincias disponibles")
 @RestController
 @RequestMapping("/api/v1/province")
 public class ControllerProvinceImpl extends ControllerTemplateImpl<Tprovince, ServiceProvinceImpl>{
