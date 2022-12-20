592322fbe48381dde0773d6ae98e89beacaa63c2
Arcelio Montezuma <arceliomonte15@gmail.com>
2022-12-19 19:11:23 -0500

    Se agrego anotaciones para configurar propiedades

--- src/main/java/com/example/RestClinicaApplication.java
+++ src/main/java/com/example/RestClinicaApplication.java
@@ -2,13 +2,19 @@ package com.example;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 
 
 @SpringBootApplication
+@CrossOrigin("*")
+@EnableAutoConfiguration
+@EnableConfigurationProperties
 @MapperScan("com.example.mapper.*")
 @Tag(name = "Clinica", description = "Manejara consultas y citas de una clinica")
 public class RestClinicaApplication {
