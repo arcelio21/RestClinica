81bde3aeca91b5428ecb0c77ae0eb322552f87ac
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-01-25 08:55:23 -0500

    Se modifico el servicio que valida usuario

--- src/main/java/com/example/service/jwt/JwtService.java
+++ src/main/java/com/example/service/jwt/JwtService.java
@@ -1,6 +1,7 @@
 package com.example.service.jwt;
 
 import com.example.dto.JwtResponse;
+import com.example.dto.TuserDto;
 import com.example.entity.user.TuserReg;
 import com.example.exception.ApiUnauthorized;
 import com.example.mapper.user.MapperUserReg;
@@ -17,7 +18,7 @@ public class JwtService {
 
     private MapperUserReg mapperUserReg;
 
-    @Value("${jwt.token.expires_in}")
+    @Value("${jwt.token.expiresIn}")
     private int EXPIRES_IN;
 
     public JwtService(JwtIo jwtIo, MapperUserReg mapperUserReg, DateUtils dateUtils) {
@@ -26,7 +27,7 @@ public class JwtService {
         this.dateUtils= dateUtils;
     }
 
-    public JwtResponse login(String cliendId, String clientSecret) throws ApiUnauthorized {
+    public JwtResponse login(String cliendId, String clientSecret)throws ApiUnauthorized {
 
         TuserReg tuserReg = new TuserReg();
         tuserReg.setIdenCard(Long.valueOf(cliendId));
@@ -38,14 +39,24 @@ public class JwtService {
             throw new ApiUnauthorized("Datos de usuario no vaidos");
         }
 
-        JwtResponse jwtResponse = JwtResponse.builder()
+        TuserDto tuserDto = TuserDto.builder()
+                .idenCard(tuserReg.getIdenCard())
+                .name(tuserReg.getName())
+                .lastName(tuserReg.getLastName())
+                .contact(tuserReg.getContact())
+                .email(tuserReg.getEmail())
+                .fechaNacimiento(tuserReg.getFechaNacimiento().toString())
+                .fechaCreacion(tuserReg.getFechaCreacion().toString())
+                .idAddress(tuserReg.getAddressId().getId())
+                .build();
+
+
+        return JwtResponse.builder()
                 .tokenType("bearer")
-                .accessToken(jwtIo.generateToken(tuserReg))
+                .accessToken(jwtIo.generateToken(tuserDto))
                 .issuedAt(dateUtils.getDateMillis()+"")
                 .clientId(cliendId)
                 .expireIn(this.EXPIRES_IN)
                 .build();
-
-        return jwtResponse;
     }
 }
