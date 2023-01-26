ad03e0c4d77343fee1cc12415272745849e22a56
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-01-26 08:49:50 -0500

    Se agrego nuevo campo a mostrar

--- src/main/java/com/example/mapper/address/MapperAddress.java
+++ src/main/java/com/example/mapper/address/MapperAddress.java
@@ -18,11 +18,12 @@ import com.example.entity.address.Taddress;
 @Mapper
 public interface MapperAddress {
 	
-	@Select("SELECT a.id as id,v.name as vname, d.name as dname, p.name as pname, specific_address " +
+	@Select("SELECT a.id as id,v.id vid, v.name as vname, d.name as dname, p.name as pname, specific_address " +
 			"FROM Taddress a INNER JOIN Tvillages v ON a.village_id=v.id " +
 			"INNER JOIN Tdistricts d ON v.district_id=d.id " +
 			"INNER JOIN Tprovinces p ON d.province_id=p.id")
 	@Result(column = "id",property = "id")
+	@Result(column = "vid", property = "villageId.id")
 	@Result(column = "vname",property = "villageId.name")
 	@Result(column = "dname", property = "villageId.district.name")
 	@Result(column = "pname",property = "villageId.district.province.name")
