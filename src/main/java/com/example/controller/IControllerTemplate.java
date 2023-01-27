abab220d14068b350565316e23f346f61a438658
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-01-27 18:25:41 -0500

    Modificacion de interfaz hecha para DTO  de respuesta

--- src/main/java/com/example/controller/IControllerTemplate.java
+++ src/main/java/com/example/controller/IControllerTemplate.java
@@ -2,15 +2,16 @@ package com.example.controller;
 
 import java.util.List;
 
+import com.example.dto.RequestResponseDTO;
 import org.springframework.http.ResponseEntity;
 
-public interface IControllerTemplate<T extends Object> {
+public interface IControllerTemplate<T> {
 
-	ResponseEntity<List<T>> getAll();
+	ResponseEntity<RequestResponseDTO> getAll();
 	
-	ResponseEntity<T> getById( Integer id);
+	ResponseEntity<RequestResponseDTO> getById( Integer id);
 	
-	ResponseEntity<Integer> save( T t);
+	ResponseEntity<RequestResponseDTO> save( T t);
 	
-	ResponseEntity<Integer> update( T t);
+	ResponseEntity<RequestResponseDTO> update( T t);
 }
