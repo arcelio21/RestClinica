d79540a0b1da824e1beddf35054d2dd9d39d10f0
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-01-28 22:13:54 -0500

    Se elimina anotacion

--- src/main/java/com/example/RestClinicaApplication.java
+++ src/main/java/com/example/RestClinicaApplication.java
@@ -2,12 +2,10 @@ package com.example;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 
 
@@ -15,7 +13,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @CrossOrigin("*")
 @EnableConfigurationProperties
 @MapperScan("com.example.mapper.*")
-@Tag(name = "Clinica", description = "Manejara consultas y citas de una clinica")
 public class RestClinicaApplication {
 
 	public static void main(String[] args) {
