8bf8b7d47d3c29f5f9890a26d37753bcd184c908
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-01-31 00:51:24 -0500

    Se crea DTO con datos para registrar usuario

---
+++ src/main/java/com/example/dto/RegisterRequest.java
@@ -0,0 +1,25 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private Long idenCard;
+    private String password;
+    private String name;
+    private String lastName;
+    private String contact;
+    private String email;
+    private LocalDate fechaNacimiento;
+    private String direcSpecific;
+    private Integer idVillage;
+}
