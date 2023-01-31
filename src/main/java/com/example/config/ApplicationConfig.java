f26b764ca6390516914988e3749a107755ba9819
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-01-31 00:44:34 -0500

    Se creo clase para creacion de Bean necesario para autenticar usuario y encryptar password

---
+++ src/main/java/com/example/config/ApplicationConfig.java
@@ -0,0 +1,48 @@
+package com.example.config;
+
+import com.example.entity.user.TuserReg;
+import com.example.mapper.user.MapperUserReg;
+import com.example.service.user.IServiceUserReg;
+import com.example.service.user.ServiceUserRegImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private  final MapperUserReg mapperUserReg;
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return cedula -> this.mapperUserReg.getByIdenCard(Long.valueOf(cedula))
+                .orElseThrow( ()-> new UsernameNotFoundException("Usuario no encontrado"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
