43fd4561cfabf057098d8c1f08c899191acfa196
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-01-31 16:09:29 -0500

    Cambiando configuracion de Open Api a anotaciones

--- src/main/java/com/example/config/OpenApiConfig.java
+++ src/main/java/com/example/config/OpenApiConfig.java
@@ -1,22 +1,22 @@
 package com.example.config;
 
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-
 @Configuration
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Rest Clinica",
+				description = "End Point para el manejo de citas y usuarios en una clinica",
+				version = "0.1",
+				contact = @Contact(
+						name = "Arcelio Montezuma",
+						email = "arceliomonte15@gmail.com"
+				)
+		)
+)
 public class OpenApiConfig {
 
-	
-	@Bean
-	public OpenAPI 	customOpenApi(){
-
-		return new OpenAPI()
-				.components(new Components())
-				.info(new Info().title("Rest Clinica")
-				.description("Api para menejo de consultas y citas en Clinica"));
-	}
 }
