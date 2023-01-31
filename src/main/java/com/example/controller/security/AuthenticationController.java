4bd3395cae33c875ca57e521724745c4eb68779a
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-01-31 08:49:28 -0500

    como obtener info del usuario autenticado

--- src/main/java/com/example/controller/security/AuthenticationController.java
+++ src/main/java/com/example/controller/security/AuthenticationController.java
@@ -3,13 +3,12 @@ package com.example.controller.security;
 import com.example.dto.AuthenticationRequest;
 import com.example.dto.AuthenticationResponse;
 import com.example.dto.RegisterRequest;
+import com.example.entity.user.TuserReg;
 import com.example.service.jwt.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -27,5 +26,16 @@ public class AuthenticationController {
     public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request){
 
         return  ResponseEntity.ok(this.authenticationService.authenticate(request));
+    }
+
+    /**
+     * Un ejemplo de como se puede utilizar la anotacion
+     * para obtener informacion del usuario autenticado
+     * @param tuserReg
+     * @return
+     */
+    @GetMapping(value = "/data")
+    public String dataUser(@AuthenticationPrincipal TuserReg tuserReg){
+        return tuserReg.getName()+" "+tuserReg.getLastName();
     }
 }
