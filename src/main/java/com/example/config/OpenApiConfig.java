0320caf1bee4cab0c19812e9d56320b3a3963bd7
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-01-31 23:49:06 -0500

    Se agrego configuracion de JWT para Open Api

--- src/main/java/com/example/config/OpenApiConfig.java
+++ src/main/java/com/example/config/OpenApiConfig.java
@@ -3,6 +3,11 @@ package com.example.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -19,4 +24,18 @@ import org.springframework.context.annotation.Configuration;
 )
 public class OpenApiConfig {
 
+	@Bean
+	public OpenAPI openAPI(){
+
+		final String securityScheme="bearerAuth";
+		return new OpenAPI()
+				.addSecurityItem(new SecurityRequirement()
+						.addList(securityScheme))
+				.components(new Components()
+						.addSecuritySchemes(securityScheme,new SecurityScheme()
+								.name(securityScheme)
+								.type(SecurityScheme.Type.HTTP)
+								.scheme("bearer")
+								.bearerFormat("JWT")));
+	}
 }
