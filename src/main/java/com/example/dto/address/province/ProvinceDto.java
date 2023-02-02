2ad682cd6925432d33c4807921cc8555f2ce6241
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-02 11:46:24 -0500

    Modificacion de atributos de ProvinceDto

--- src/main/java/com/example/dto/address/province/ProvinceDto.java
+++ src/main/java/com/example/dto/address/province/ProvinceDto.java
@@ -1,17 +1,22 @@
 package com.example.dto.address.province;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.SchemaProperty;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 
 @Builder
 @Getter
 @Schema(name = "ProvinceDto",description = "Estructura de respuesta de los datos")
 public class  ProvinceDto{
 
-    @Schema(description = "ID de la province",format = "int32", minimum = "1", example = "1")
-    Integer ID;
+    @SchemaProperty(name = "id", schema = @Schema(description = "ID de la province",format = "int32", minimum = "1", example = "1"))
+    private  Integer id;
     @Schema(description = "Nombre de la provincia", example = "Chiriqui")
-    String nombre;
+    @NotEmpty(message = "Campo no puede estar vacio")
+    @Min(value = 4, message = "Valor debe ser de minimo 4 caracteres")
+    private String nombre;
 }
