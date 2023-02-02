0bf3d6ae0a0cf1eeccca50e88511c5c0cbc38127
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-01 22:53:18 -0500

    Se agrego atributo para mapear DTOs

--- src/main/java/com/example/service/address/ServiceProvinceImpl.java
+++ src/main/java/com/example/service/address/ServiceProvinceImpl.java
@@ -1,55 +1,78 @@
 package com.example.service.address;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import com.example.dto.address.province.ProvinceDto;
+import com.example.dtomapper.address.ProvinceMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.entity.address.Tprovince;
 import com.example.mapper.address.MapperProvince;
 
+@RequiredArgsConstructor
 @Service
-public class ServiceProvinceImpl implements IServiceProvince<Tprovince, Integer> {
+public class ServiceProvinceImpl implements IServiceProvince<ProvinceDto, Integer> {
 
-	private MapperProvince mapperProvince;
+
+	private final MapperProvince mapperProvince;
+	@Autowired
+	private final ProvinceMapper provinceMapper;
 	
-	public ServiceProvinceImpl(MapperProvince mapperProvince) {
-		this.mapperProvince=mapperProvince;
-	}
-	
+
 	/**
 	 * SE EJECUTARA UN QUERY QUE TRAERA LAS PROVINCIAS, CON
 	 * LOS DISTRITOS ASOCIADO A CADA PROVINCIA Y LOS CORREGIMIENTOS ASOCIADO A CADA DISTRITOS
 	 */
 	@Override
-	public List<Tprovince> getAll() {
-		return this.mapperProvince.getAll();
+	public List<ProvinceDto> getAll() {
+		List<Tprovince> tprovinces = this.mapperProvince.getAll();
+		if(tprovinces==null || tprovinces.isEmpty()){
+			return Collections.emptyList();
+		}
+		List<ProvinceDto> provinceDtos = new ArrayList<>();
+		for (Tprovince province:
+			 tprovinces) {
+			provinceDtos.add(this.provinceMapper.tprovinceToProvinceDto(province));
+		}
+		return provinceDtos;
 	}
 
 	/**
 	 * SE OBTENDRA UN PROVINCIA POR ID 
 	 */
 	@Override
-	public Tprovince getById(Integer id) {
+	public ProvinceDto getById(Integer id) {
 		if(id==null || id==0) {
 			return null;
 		}
-		return this.mapperProvince.getById(id);
+		return this.provinceMapper.tprovinceToProvinceDto(this.mapperProvince.getById(id));
 	}
 
 	@Override
-	public Integer update(Tprovince tprovince) {
+	public Integer update(ProvinceDto provinceDto) {
 		
-		if(tprovince==null || tprovince.getId()==null || tprovince.getId()==0) {
+		if(provinceDto==null || provinceDto.getID()==null || provinceDto.getID()==0) {
 			return 0;
 		}
+
+		Tprovince tprovince = this.provinceMapper.provinceDtoToTprovince(provinceDto);
 		
 		return this.mapperProvince.update(tprovince);
 	}
 
 	@Override
-	public Integer save(Tprovince tprovince) {
+	public Integer save(ProvinceDto provinceDto) {
 
+		if(provinceDto==null || provinceDto.getNombre()==null || provinceDto.getNombre().isEmpty()){
+			return 0;
+		}
+
+		Tprovince tprovince =this.provinceMapper.provinceDtoToTprovince(provinceDto);
 		return this.mapperProvince.save(tprovince);
 	}
 
@@ -57,8 +80,8 @@ public class ServiceProvinceImpl implements IServiceProvince<Tprovince, Integer>
 	 * TRAERA LA INFORMACION MAS SIMPLE DE UNA PROVINCIA, SU NOMBRE E ID
 	 */
 	@Override
-	public List<Tprovince> getAllSimple() {
-		return this.mapperProvince.getAll();
+	public List<ProvinceDto> getAllSimple() {
+		return null;
 	}
 
 	@Override
