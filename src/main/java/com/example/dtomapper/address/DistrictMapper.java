ecc49050e80975d5be4518745e1315cb2abdc53f
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-02 14:45:41 -0500

    Se agrego metodo de conversion para distritos

--- src/main/java/com/example/dtomapper/address/DistrictMapper.java
+++ src/main/java/com/example/dtomapper/address/DistrictMapper.java
@@ -1,5 +1,6 @@
 package com.example.dtomapper.address;
 
+import com.example.dto.address.district.DistrictAllDto;
 import com.example.dto.address.district.DistrictDto;
 import com.example.entity.address.Tdistrict;
 import org.mapstruct.Mapper;
@@ -26,5 +27,24 @@ public interface DistrictMapper {
             @Mapping(target = "province.id", source = "districtDto.provinceId", defaultValue = "0"),
             @Mapping(target = "province", ignore = true)
     })
-    Tdistrict districtDtoToTprovince(DistrictDto districtDto);
+    Tdistrict districtDtoToTdistrict(DistrictDto districtDto);
+
+    @Mappings({
+            @Mapping(source = "tdistrict.id", target = "id"),
+            @Mapping(source = "tdistrict.name", target = "name"),
+            @Mapping(source = "tdistrict.province.id", target = "tprovince.id"),
+            @Mapping(source = "tdistrict.province.name", target = "tprovince.nombre"),
+            @Mapping(target = "tprovince", ignore = true)
+    })
+    DistrictAllDto tdistrictToDistrictAll(Tdistrict tdistrict);
+
+    @Mappings(value = {
+            @Mapping(target = "id", source = "districtAllDto.id"),
+            @Mapping(target = "name", source = "districtAllDto.name"),
+            @Mapping(target = "villages", ignore = true),
+            @Mapping(target = "province.id", source = "districtAllDto.tprovince.id"),
+            @Mapping(target = "province.name", source = "districtAllDto.tprovince.nombre"),
+            @Mapping(target = "province", ignore = true)
+    })
+    Tdistrict districtALlToTdistrict(DistrictAllDto districtAllDto);
 }
