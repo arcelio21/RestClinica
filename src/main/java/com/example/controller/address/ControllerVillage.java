9f4837c289e33ac045e8e0b72d9436ff429d5372
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-03 11:44:55 -0500

    Metodo de actualizacion y guardado creado para controlador de village

--- src/main/java/com/example/controller/address/ControllerVillage.java
+++ src/main/java/com/example/controller/address/ControllerVillage.java
@@ -14,10 +14,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/village")
@@ -51,7 +48,7 @@ public class ControllerVillage {
     @Operation(summary = "Obtener village por ID",description = "Se podra obtener el village que se desee filtrandolo por su ID",
             method = "GET", responses = {
             @ApiResponse(responseCode = "200",description = "Village encontrado",
-                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = DistrictDto.class,description = "Datos de Distrito"))),
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = VillageDto.class,description = "Datos de Distrito"))),
             @ApiResponse(responseCode = "404",description = "Village no encontrado, Id no valido",content = @Content(schema = @Schema))
     },parameters = {
             @Parameter(name = "id", in = ParameterIn.PATH, description = "ID de recurso",example = "1",required = true, schema = @Schema(implementation = Integer.class,type = "integer", format = "int32"))
@@ -60,6 +57,52 @@ public class ControllerVillage {
     public ResponseEntity<VillageDto> getById(@PathVariable("id") Integer id){
         return ResponseEntity.ok(
                 this.serviceVillage.getById(id)
+        );
+    }
+
+    @Operation(
+            summary = "Guardar nuevo village",
+            description = "Se guardara una nuevo village en caso de necesitarse",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Village creado correctamente",content = {
+                            @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ResponseDTO.class))
+                    }),
+                    @ApiResponse(responseCode = "400", description = "Datos proporcionado no son validos",content = {
+                            @Content(schema = @Schema)
+                    })
+            }
+    )
+    @PostMapping
+    public ResponseEntity<ResponseDTO> save(@RequestBody VillageDto villageDto){
+        return ResponseEntity.ok(
+                ResponseDTO.builder()
+                        .info("Cantidad de registro creados")
+                        .data(this.serviceVillage.save(villageDto))
+                        .build()
+        );
+    }
+
+    @Operation(
+            summary = "Actualizar village",
+            description = "Se actualizara village que se desee",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Village actualizado correctamente",content = {
+                            @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ResponseDTO.class))
+                    }),
+                    @ApiResponse(responseCode = "400", description = "Datos proporcionado no son validos",content = {
+                            @Content(schema = @Schema)
+                    })
+            }
+    )
+    @PutMapping
+    public ResponseEntity<ResponseDTO> update(@RequestBody VillageDto villageDto){
+        return ResponseEntity.ok(
+                ResponseDTO.builder()
+                        .info("Cantidad de registros actualizados")
+                        .data(this.serviceVillage.update(villageDto))
+                        .build()
         );
     }
 }
