40c0b7070b6c3107f0e01930897ab6439c34942b
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-03 14:40:03 -0500

    Se agrego metodos en interface de service a utilizar

--- src/main/java/com/example/service/address/IServiceVillage.java
+++ src/main/java/com/example/service/address/IServiceVillage.java
@@ -2,12 +2,19 @@ package com.example.service.address;
 
 import java.util.List;
 
+import com.example.dto.address.village.VillageDistrictDto;
+import com.example.dto.address.village.VillageDto;
 import com.example.entity.address.Tdistrict;
 import com.example.entity.address.Tvillage;
 import com.example.service.ServiceTemplateCrud;
 
 public interface IServiceVillage<T,I> extends ServiceTemplateCrud<T, I>{
 
-	List<Tvillage> getByDistrictId( Tdistrict tdistrict);
-	Tvillage getDistrictAllById( I id);
+	List<T> getByDistrictId(I id);
+	VillageDistrictDto getDistrictAllById( I id);
+
+	List<T> getAllIdName();
+
+	T getByIdName(I id);
+
 }
