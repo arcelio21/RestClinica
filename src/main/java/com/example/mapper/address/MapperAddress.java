026ea1f51f1cb63f47c4b90705b6ff824eb8d085
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-05 15:55:12 -0500

    Se modifico las query permitidas a la bd de Tadress

--- src/main/java/com/example/mapper/address/MapperAddress.java
+++ src/main/java/com/example/mapper/address/MapperAddress.java
@@ -1,38 +1,28 @@
 package com.example.mapper.address;
 
-import java.util.List;
-
-import org.apache.ibatis.annotations.*;
-import org.apache.ibatis.mapping.FetchType;
-
 import com.example.entity.address.Taddress;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface MapperAddress {
-	
+
 	@Select("SELECT a.id as id,v.id vid, v.name as vname, d.name as dname, p.name as pname, specific_address " +
 			"FROM Taddress a INNER JOIN Tvillages v ON a.village_id=v.id " +
 			"INNER JOIN Tdistricts d ON v.district_id=d.id " +
 			"INNER JOIN Tprovinces p ON d.province_id=p.id")
 	@Results(
 			id = "address", value = {
-			@Result(column = "id",property = "id"),
+			@Result(column = "id", property = "id"),
 			@Result(column = "vid", property = "villageId.id"),
-			@Result(column = "vname",property = "villageId.name"),
+			@Result(column = "vname", property = "villageId.name"),
 			@Result(column = "dname", property = "villageId.district.name"),
-			@Result(column = "pname",property = "villageId.district.province.name"),
+			@Result(column = "pname", property = "villageId.district.province.name"),
 			@Result(column = "specific_address", property = "specificAddress")
-		}
+	}
 	)
-	public List<Taddress> getAll();
-	
-	@Select("SELECT * FROM Taddress WHERE id=#{id}")
-	@Results(id = "addressAll",value={@Result(column = "id",property = "id"),
-			@Result(column = "specific_address",property = "specificAddress"),
-			@Result(column="village_id",property = "villageId",one = @One(select = "com.example.mapper.address.MapperVillage.getById",
-					fetchType = FetchType.LAZY))}
-	)
-	public Taddress getByIdAll(@Param("id") Integer id);
+	List<Taddress> getAll();
 
 	@Select("SELECT a.id as id,v.id vid, v.name as vname, d.name as dname, p.name as pname, specific_address " +
 			"FROM Taddress a INNER JOIN Tvillages v ON a.village_id=v.id " +
@@ -40,24 +30,14 @@ public interface MapperAddress {
 			"INNER JOIN Tprovinces p ON d.province_id=p.id " +
 			"WHERE a.id=#{id}")
 	@ResultMap(value = "address")
-	public Taddress getById(@Param("id") Integer id);
-	
-	
-	@Select("SELECT a.id as id,a.specific_address as 'specific',v.id as villId,v.name as villName FROM Taddress a "
-			+ "INNER JOIN Tvillages v  ON a.village_id=v.id WHERE a.id=#{id}")
-	@Result(column = "id",property = "id")
-	@Result(column = "specific",property = "specificAddress")
-	@Result(column = "villId",property = "villageId.id")
-	@Result(column = "villName",property = "villageId.name")
-	public Taddress getVillageAllById(@Param("id") Integer id);
-	
+	Taddress getById(@Param("id") Integer id);
 
 	@Insert("INSERT INTO Taddress (specific_address,village_id) VALUES(#{addr.specificAddress},#{addr.villageId.id})")
-	@Options(useGeneratedKeys = true,keyProperty = "id",keyColumn = "id")
-	public int save(@Param("addr")Taddress address);
+	@Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+	int save(@Param("addr")Taddress address);
 	
 	
-	@Update("UPDATE Tvillages SET specific_address=#{addr.specificAddress},village_id=#{addr.villageId.id} WHERE id=#{addr.id}")
-	public int update(@Param("addr") Taddress address);
+	@Update("UPDATE Taddress SET specific_address=#{addr.specificAddress},village_id=#{addr.villageId.id} WHERE id=#{addr.id}")
+	int update(@Param("addr") Taddress address);
 
 }
