71ce799fdb8283bb93220baa8c41fe62a82ac333
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-07 17:38:38 -0500

    Se definio el mensaje de la excepcion

--- src/main/java/com/example/exception/NoDataFoundException.java
+++ src/main/java/com/example/exception/NoDataFoundException.java
@@ -1,4 +1,12 @@
 package com.example.exception;
 
-public class NoDataFoundException {
+public class NoDataFoundException extends RuntimeException{
+
+    public NoDataFoundException(Integer id) {
+        super(String.format("Data con id %d no encontrado", id));
+    }
+
+    public NoDataFoundException() {
+        super("Data no encontrada");
+    }
 }
