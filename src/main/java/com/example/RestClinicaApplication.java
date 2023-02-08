0b6351ac61943f92e04e3ade60fa6e5117d2dde3
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-08 00:52:26 -0500

    Se agrego librerias y anotacion necesaria para implementar aspecto con metodo para calcular tiempo de ejecucion de un metodo

--- src/main/java/com/example/RestClinicaApplication.java
+++ src/main/java/com/example/RestClinicaApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("*")
 @EnableConfigurationProperties
 @MapperScan("com.example.mapper.*")
+@EnableAspectJAutoProxy
 public class RestClinicaApplication {
 
 	public static void main(String[] args) {
