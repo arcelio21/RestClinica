bf2234a30a8eb0d3bb24c75cfd206aba6520b1db
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-13 12:40:00 -0500

    Se implemento programacion funcional en todos los metodos

--- src/main/java/com/example/service/user/ServiceUserRegImpl.java
+++ src/main/java/com/example/service/user/ServiceUserRegImpl.java
@@ -1,86 +1,84 @@
 package com.example.service.user;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-
+import com.example.dto.AuthenticationRequest;
+import com.example.dto.user.UserRegDto;
+import com.example.dto.user.UserUpdatePassDto;
+import com.example.dtomapper.user.UserRegMapper;
 import com.example.exception.NoDataFoundException;
+import com.example.mapper.user.MapperUserReg;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import com.example.entity.user.TuserReg;
-import com.example.mapper.user.MapperUserReg;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ServiceUserRegImpl implements IServiceUserReg{
 
 	private final MapperUserReg mapperUserReg;
+	private final UserRegMapper userRegMapper;
 
 	
 	@Override
-	public List<TuserReg> getAll() {
+	public List<UserRegDto> getAll() {
 
-		//TODO Hacer DTO para mapear estos datos
-		return Optional.ofNullable(this.mapperUserReg.getAll()).
-				orElseThrow(NoDataFoundException::new);
+		return Optional.ofNullable(this.mapperUserReg.getAll())
+				.orElseThrow(NoDataFoundException::new)
+				.stream()
+				.map(this.userRegMapper::TuserRegToUserRegDto)
+				.toList();
 	}
 
 	@Override
-	public TuserReg getById(Integer id) {
-		return Optional.ofNullable(id).map(this.mapperUserReg::getById).orElse(null);
+	public UserRegDto getById(Integer id) {
+		return Optional.ofNullable(id)
+				.map(this.mapperUserReg::getById)
+				.map(this.userRegMapper::TuserRegToUserRegDto)
+				.orElseThrow(() -> new NoDataFoundException(id));
 	}
 
 	@Override
-	public Integer update(TuserReg t) {
-		
+	public Integer update(UserRegDto t) {
+
 		if(t==null || t.getId()==null) {
 			return 0;
 		}
-		
-		Optional<TuserReg> user=Optional.ofNullable(t.getId()).map(this.mapperUserReg::getById);
-		if(user.isEmpty()) {
-			//System.out.println(user);
-			return 0;
-		}
-		
-		try {
-			return Optional.ofNullable(t).map(this.mapperUserReg::update).orElse(0);
-		}catch (Exception e) {
-			return 0;
-		}
-		
+
+		return Optional.of(t)
+				.map(this.userRegMapper::userRegDtoToTuserReg)
+				.map(this.mapperUserReg::update)
+				.orElse(0);
 		
 	}
 	
 	@Override
-	public Integer save(TuserReg t) {
+	public Integer save(UserRegDto t) {
 		
-		if(t==null ) {
-			return 0;
-		}
-		
-		
-		return Optional.ofNullable(t).map(this.mapperUserReg::save).orElse(0);
+		return Optional.ofNullable(t)
+				.map(this.userRegMapper::userRegDtoToTuserReg)
+				.map(this.mapperUserReg::save)
+				.orElse(0);
 	}
 
 	@Override
-	public List<TuserReg> getByName(String name) {
+	public List<UserRegDto> getByName(String name) {
 		
 	  if(name==null || name.trim().equals("")) {
-		  return Collections.emptyList();
+		  throw new NoDataFoundException();
 	  }
-	  List<TuserReg> users=this.getByName(name);
-	  if(users==null || users.isEmpty()) {
-		  return Collections.emptyList();
-	  }
-		  
-		
-	  return users;
+
+	  return Optional.of(name)
+			  .map(this.mapperUserReg::getByName)
+			  .orElseThrow(NoDataFoundException::new)
+			  .stream()
+			  .map(this.userRegMapper::TuserRegToUserRegDto)
+			  .toList();
 	}
 
 	@Override
-	public TuserReg validateAccount(TuserReg user) {
+	public UserRegDto validateAccount(AuthenticationRequest user) {
 		
 		if(user==null || user.getIdenCard()==null || user.getPassword()==null) {
 			return null;
@@ -88,20 +86,36 @@ public class ServiceUserRegImpl implements IServiceUserReg{
 		
 		if(user.getPassword().trim().equals("")) {
 			user.setPassword("PASS VACIO");
-			return user;
+			return null;
 		}
 		
-		return this.mapperUserReg.validateAccount(user);
+		return Optional.of(user)
+				.map(this.userRegMapper::authenticationRequestToTuserReg)
+				.map(this.mapperUserReg::validateAccount)
+				.map(this.userRegMapper::TuserRegToUserRegDto)
+				.orElseThrow();
 	}
 
 	@Override
-	public Integer updatePassword(TuserReg user) {
+	public Integer updatePassword(UserUpdatePassDto user) {
 		
-		if(user==null || user.getPassword()==null ||user.getPassword().trim().equals("") || user.getIdenCard()==null){
+		if(user==null ){
 			return 0;
 		}
-		
-		return this.mapperUserReg.updatePassword(user);
+		List<String> properties = Arrays.asList(user.getOldPassword(), user.getIndeCard().toString(), user.getNewPassword());
+		long count = properties.stream()
+				.filter(p -> p != null && !p.trim().equals(""))
+				.count();
+
+
+
+		return (count== properties.size())?
+					Optional.of(user)
+					.map(this.userRegMapper::userUpdatePassToTuserReg)
+					.map(tuserReg -> this.mapperUserReg.updatePassword(tuserReg, user.getNewPassword()))
+					.orElseThrow()
+					: 0;
+
 	}
 
 }
