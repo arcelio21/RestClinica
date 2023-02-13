f85c8dc6d50b6f1130823cbe4773fc90e00ea16c
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-13 12:36:37 -0500

    Se elimino modificadores de acceso de metodos y se agrego argumentos de para actualizacion de contraseña

--- src/main/java/com/example/mapper/user/MapperUserReg.java
+++ src/main/java/com/example/mapper/user/MapperUserReg.java
@@ -17,49 +17,48 @@ import com.example.entity.user.TuserReg;
 @Mapper
 public interface MapperUserReg {
 
-	
+
 	@Select("SELECT * FROM TusersRegs")
-	@Results(id = "userRegSimple",value= {
-		@Result(column = "id",property = "id"),
-		@Result(column = "iden_card",property = "idenCard"),
-		@Result(column = "name",property = "name"),
-		@Result(column = "last_name",property = "lastName"),
-		@Result(column = "contact",property = "contact"),
-		@Result(column = "email",property = "email"),
-		@Result(column = "fecha_nacimiento",property = "fechaNacimiento"),
-		@Result(column = "password",property = "password"),
-		@Result(column = "fecha_creacion",property = "fechaCreacion"),
-		@Result(column = "address_id",property = "addressId.id")
+	@Results(id = "userRegSimple", value = {
+			@Result(column = "id", property = "id"),
+			@Result(column = "iden_card", property = "idenCard"),
+			@Result(column = "name", property = "name"),
+			@Result(column = "last_name", property = "lastName"),
+			@Result(column = "contact", property = "contact"),
+			@Result(column = "email", property = "email"),
+			@Result(column = "fecha_nacimiento", property = "fechaNacimiento"),
+			@Result(column = "fecha_creacion", property = "fechaCreacion"),
+			@Result(column = "address_id", property = "addressId.id")
 	})
-	public List<TuserReg> getAll();
-	
-	
+	List<TuserReg> getAll();
+
+
 	@Select("SELECT * FROM TusersRegs WHERE id=#{id}")
 	@ResultMap(value = "userRegSimple")
-	public TuserReg getById(@Param("id") Integer id);
+	TuserReg getById(@Param("id") Integer id);
 
 	@Select("SELECT * FROM TusersRegs WHERE iden_card=#{idencard}")
 	@ResultMap(value = "userRegSimple")
-	public Optional<TuserReg> getByIdenCard(@Param("idencard") Long idenCard);
-	
+	Optional<TuserReg> getByIdenCard(@Param("idencard") Long idenCard);
+
 	@Select("SELECT * FROM TusersRegs WHERE name LIKE '${name}%' ")
 	@ResultMap(value = "userRegSimple")
-	public List<TuserReg> getByName(@Param("name") String name);
+	List<TuserReg> getByName(@Param("name") String name);
 	
 	
 	@Insert("INSERT INTO TusersRegs(iden_card,name,last_name,contact,email,fecha_nacimiento,password,address_id)"
 			+ " VALUES(#{user.idenCard},#{user.name},#{user.lastName},#{user.contact},#{user.email},#{user.fechaNacimiento},"
 			+ "#{user.password},#{user.addressId.id}) ")
-	public Integer save(@Param("user") TuserReg user);
+	Integer save(@Param("user") TuserReg user);
 	
 	
 	@Update("UPDATE TusersRegs SET iden_card=#{user.idenCard},name=#{user.name},last_name=#{user.lastName},contact=#{user.contact},"
 			+ "email=#{user.email},fecha_nacimiento=#{user.fechaNacimiento},address_id=#{user.addressId.id} WHERE id=#{user.id}")
-	public Integer update(@Param("user") TuserReg user);
+	Integer update(@Param("user") TuserReg user);
 	
 	
-	@Update("UPDATE TusersRegs SET password=SHA1('${user.password}') WHERE id=${user.id} OR iden_card=${user.idenCard}")
-	public Integer updatePassword(@Param("user") TuserReg user);
+	@Update("UPDATE TusersRegs SET password=${newPass} WHERE iden_card=${user.idenCard} AND password=${user.password}")
+	Integer updatePassword(@Param("user") TuserReg user, @Param("newPass") String newPass);
 	
 	
 	@Select("SELECT * FROM TusersRegs WHERE iden_card=${user.idenCard} AND password=sha1('${user.password}') ")
@@ -72,5 +71,5 @@ public interface MapperUserReg {
 	@Result(column = "fecha_nacimiento",property = "fechaNacimiento")
 	@Result(column = "fecha_creacion",property = "fechaCreacion")
 	@Result(column = "address_id",property = "addressId.id")
-	public TuserReg validateAccount(@Param("user") TuserReg user);
+	TuserReg validateAccount(@Param("user") TuserReg user);
 }
