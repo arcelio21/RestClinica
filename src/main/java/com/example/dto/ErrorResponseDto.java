99895cb7f1ab12d5c1149b94d2466f0554197b40
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-13 17:03:12 -0500

    Se creo Dto para mostrar datos de error

---
+++ src/main/java/com/example/dto/ErrorResponseDto.java
@@ -0,0 +1,18 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponseDto {
+
+    private LocalDate fecha;
+    private String messageError;
+}
