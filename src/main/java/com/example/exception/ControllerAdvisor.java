031cf3ff240a67433eeb97fdfe207a505be0eac3
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-13 15:04:42 -0500

    Se creo manejador de excepcion para usuario no valido

--- src/main/java/com/example/exception/ControllerAdvisor.java
+++ src/main/java/com/example/exception/ControllerAdvisor.java
@@ -1,5 +1,6 @@
 package com.example.exception;
 
+import com.example.exception.user.UsernameInvalid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +9,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -15,12 +17,21 @@ import java.util.Map;
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NoDataFoundException.class)
-    public ResponseEntity<Object> handleNoDatFoundException(NoDataFoundException ex, WebRequest request){
+    public ResponseEntity<Object> handleNoDatFoundException(NoDataFoundException ex){
 
         Map<String,Object> error=new LinkedHashMap<>();
         error.put("Fecha", LocalDate.now());
         error.put("Message", ex.getMessage());
 
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UsernameInvalid.class)
+    public ResponseEntity<Object> handleUsernameInvalid(UsernameInvalid ex){
+        Map<String, Object> error = new HashMap<>();
+        error.put("Fecha", LocalDate.now());
+        error.put("Message", ex.getMessage());
+
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
 }
