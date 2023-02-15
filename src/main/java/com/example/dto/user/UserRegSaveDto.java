6fb28d096c11e84d7a018aa02a68b5cc22a707f0
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-14 19:37:26 -0500

    Solucion de como tener un builder de un padre e hija

---
+++ src/main/java/com/example/dto/user/UserRegSaveDto.java
@@ -0,0 +1,30 @@
+package com.example.dto.user;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Schema(name = "UserRegSave", description = "Dto utilizado para guardar info de usuario")
+public class UserRegSaveDto extends UserRegDto{
+
+    //@Schema(accessMode = Schema.AccessMode.WRITE_ONLY)
+    private Long villageId;
+
+    @Schema(accessMode = Schema.AccessMode.WRITE_ONLY)
+    private String direcSpecific;
+
+
+    @Builder
+    public UserRegSaveDto(Integer id, Long idenCard, String name, String lastName, String password, String contact, String email,
+                          LocalDate fechaNacimiento, LocalDateTime fechaCreacion, Integer addressId,
+                            Long villageId, String direcSpecific
+    ) {
+        super(id, idenCard, name, lastName, password, contact, email, fechaNacimiento, fechaCreacion, addressId);
+        this.villageId= villageId;
+        this.direcSpecific=direcSpecific;
+    }
+}
