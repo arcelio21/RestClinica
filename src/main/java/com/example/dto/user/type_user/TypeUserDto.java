5f2ee06ea5096fc278441b3538e870295088b80b
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-27 14:00:39 -0500

    Se incluyo anotaciones para validar datos

--- src/main/java/com/example/dto/user/type_user/TypeUserDto.java
+++ src/main/java/com/example/dto/user/type_user/TypeUserDto.java
@@ -4,14 +4,22 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Builder
 @Getter
 @Schema(name = "TypeUserGet", description = "Tendra informacion de los tipos de usuario")
 public class TypeUserDto {
 
+
+    @NotNull(message = "ID no puede ser nulo ni vacio")
+    @Min(value = 1, message = "El valor no puede ser menor a 1")
     @Schema(name = "id",description = "Identifacion unica de tipo de usuario", example = "1")
     private Integer id;
 
     @Schema(name = "name", description = "Nombre del tipo de usuario", defaultValue = "Administrador")
+    @NotBlank(message = "Atributo no debe contener cadena vacia")
     private String name;
 }
