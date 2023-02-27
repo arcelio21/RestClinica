392ff3e23ea123943adcbd1f1e252b71e601eb43
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-27 17:51:46 -0500

    Se creo mapeador de objetos dto a entidad de tipos de usuarios

---
+++ src/main/java/com/example/dtomapper/user/TypeUserMapper.java
@@ -0,0 +1,26 @@
+package com.example.dtomapper.user;
+
+import com.example.dto.user.type_user.TypeUserDto;
+import com.example.entity.user.TtypeUser;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface TypeUserMapper {
+
+    TypeUserMapper INSTANCE = Mappers.getMapper(TypeUserMapper.class);
+
+    @Mappings({
+            @Mapping(source = "tUser.id", target = "id"),
+            @Mapping(source = "tUser.nameTypeUser", target = "name")
+    })
+    TypeUserDto ttypeUserToTypeUserDto(TtypeUser tUser);
+
+    @Mappings({
+            @Mapping(source = "tUserDto.id", target = "id"),
+            @Mapping(source = "tUserDto.name", target = "nameTypeUser")
+    })
+    TtypeUser typeUserDtoToTtypeUser(TypeUserDto tUserDto);
+}
