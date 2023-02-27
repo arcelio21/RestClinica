bf66a002e9738a7f027085115987dbd294e7ba9b
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-27 18:00:50 -0500

    Se modifico la implementacion de los metodos de interacion de los datos de tipos de usuarios

--- src/main/java/com/example/service/user/ServiceTypeUserImpl.java
+++ src/main/java/com/example/service/user/ServiceTypeUserImpl.java
@@ -1,15 +1,17 @@
 package com.example.service.user;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
-import com.example.entity.user.TtypeUser;
+import com.example.dto.user.type_user.TypeUserDto;
+import com.example.dtomapper.user.TypeUserMapper;
+import com.example.exception.NoDataFoundException;
+import com.example.exception.user.type_user.TypeUserNotSaveException;
+import com.example.exception.user.type_user.TypeUserNotUpdateException;
 import com.example.mapper.user.MapperTypeUser;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -18,43 +20,54 @@ public class ServiceTypeUserImpl implements IServiceTypeUser{
 
 	
 	private final MapperTypeUser mapperTypeUser;
+	private final TypeUserMapper typeUserMapper;
 
 	@Override
-	public List<TtypeUser> getAll() {
-		return Collections.emptyList();
+	public List<TypeUserDto> getAll() {
+
+		return Optional.ofNullable(this.mapperTypeUser.getAll())
+				.orElseThrow(NoDataFoundException::new)
+				.stream()
+				.map(this.typeUserMapper::ttypeUserToTypeUserDto)
+				.toList();
 	}
 
 	@Override
-	public TtypeUser getById(Integer id) {
+	public TypeUserDto getById(Integer id) {
 
 		if(id==null || id<=0) {
-			return null;
+			throw new NoDataFoundException("Valor de ID no valido");
 		}
 		
-		return this.mapperTypeUser.getById(id);
+		return Optional.ofNullable(this.mapperTypeUser.getById(id))
+				.map(this.typeUserMapper::ttypeUserToTypeUserDto)
+				.orElseThrow(()-> new NoDataFoundException(id));
 	}
 
 	@Override
-	public Integer update(TtypeUser t) {
+	public Integer update(TypeUserDto t) {
 
 		if(t==null || t.getId()==null || t.getId()<=0) {
-			return 0;
+			throw new TypeUserNotUpdateException("Datos no validos", t);
 		}
 		
-		if(this.mapperTypeUser.getById(t.getId())==null ) {
-			return 0;
-		}
-		
-		return this.mapperTypeUser.update(t);
+		return Optional.of(t)
+				.map(this.typeUserMapper::typeUserDtoToTtypeUser)
+				.map(this.mapperTypeUser::update)
+				.orElseThrow(() -> new TypeUserNotUpdateException("Error al intentar actualizar, Datos no validos",t));
 	}
 
 	@Override
-	public Integer save(TtypeUser t) {
+	public Integer save(TypeUserDto t) {
 
-		if(t==null ) {
-			return 0;
+		if(t==null || t.getName().trim().equals("") ) {
+			throw new TypeUserNotSaveException("Datos no validos", t);
 		}
-		return this.mapperTypeUser.save(t);
+
+		return Optional.of(t)
+				.map(this.typeUserMapper::typeUserDtoToTtypeUser)
+				.map(this.mapperTypeUser::save)
+				.orElseThrow(()-> new TypeUserNotSaveException("Error al intentar guardar", t));
 	}
 
 
