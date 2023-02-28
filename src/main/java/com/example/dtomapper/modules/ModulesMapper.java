c6e09dd2066b1e5d134dfff4a57ae18d71611167
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-02-27 20:54:16 -0500

    Se creo MapperDto para modules

---
+++ src/main/java/com/example/dtomapper/modules/ModulesMapper.java
@@ -0,0 +1,27 @@
+package com.example.dtomapper.modules;
+
+import com.example.dto.modules.ModulesDto;
+import com.example.entity.modules.Tmodule;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ModulesMapper {
+
+    ModulesMapper INSTANCE = Mappers.getMapper(ModulesMapper.class);
+
+    @Mappings({
+            @Mapping(source = "module.id", target = "id"),
+            @Mapping(source = "module.nameModule", target = "name")
+    })
+    ModulesDto TmoduleToModulesDto(Tmodule module);
+
+
+    @Mappings({
+            @Mapping(source = "module.id", target = "id"),
+            @Mapping(source = "module.name", target = "nameModule")
+    })
+    Tmodule modulesDtoToTmodule(ModulesDto module);
+}
