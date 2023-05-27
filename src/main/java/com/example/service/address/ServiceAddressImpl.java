38bef73345ffe169f206f9dcecb03ea818862222
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-05-27 18:31:52 -0500

    Se agrego documentacion a clase de servicio de address

--- src/main/java/com/example/service/address/ServiceAddressImpl.java
+++ src/main/java/com/example/service/address/ServiceAddressImpl.java
@@ -1,31 +1,39 @@
 package com.example.service.address;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import com.example.dto.address.AddressGetDto;
 import com.example.dto.address.AddressRequestDto;
 import com.example.dtomapper.address.AddressMappper;
+import com.example.entity.address.Taddress;
 import com.example.exception.NoDataFoundException;
 import com.example.exception.address.AddressNotSaveException;
 import com.example.exception.address.AddressNotUpdateException;
 import com.example.exception.address.province.ProvinceNotSaveException;
 import com.example.exception.address.province.ProvinceNotUpdateException;
+import com.example.mapper.address.MapperAddress;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import com.example.entity.address.Taddress;
-import com.example.mapper.address.MapperAddress;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Implementación de la interfaz IServiceAddress que proporciona métodos para operaciones relacionadas con direcciones.
+ */
 @RequiredArgsConstructor
 @Service
 public class ServiceAddressImpl implements IServiceAddress{
 
 	private final MapperAddress mapper;
 	private final AddressMappper addressMappper;
-	
 
+
+	/**
+	 * Obtiene una lista de todas las direcciones disponibles.
+	 *
+	 * @return Una lista de objetos AddressGetDto que representan las direcciones disponibles.
+	 * @throws NoDataFoundException Si no se encuentran datos de direcciones disponibles.
+	 */
 	@Override
 	public List<AddressGetDto> getAll() {
 
@@ -42,6 +50,13 @@ public class ServiceAddressImpl implements IServiceAddress{
 		throw new NoDataFoundException();
 	}
 
+	/**
+	 * Obtiene un objeto AddressGetDto por su identificador.
+	 *
+	 * @param id El identificador de la dirección.
+	 * @return Un objeto AddressGetDto que representa la dirección encontrada.
+	 * @throws NoDataFoundException Si no se encuentra ninguna dirección para el identificador especificado.
+	 */
 	@Override
 	public AddressGetDto getById(Integer id) {
 
@@ -56,6 +71,13 @@ public class ServiceAddressImpl implements IServiceAddress{
 		
 	}
 
+	/**
+	 * Actualiza una dirección existente con la información proporcionada en el objeto AddressRequestDto.
+	 *
+	 * @param addressRequestDto El objeto AddressRequestDto con la información de la dirección a actualizar.
+	 * @return El identificador de la dirección actualizada.
+	 * @throws AddressNotUpdateException Si los datos de la dirección no son válidos para la actualización.
+	 */
 	@Override
 	public Integer update(AddressRequestDto addressRequestDto) {
 		
@@ -72,6 +94,13 @@ public class ServiceAddressImpl implements IServiceAddress{
 
 	}
 
+	/**
+	 * Guarda un objeto AddressRequestDto en el sistema.
+	 *
+	 * @param addressRequestDto El objeto AddressRequestDto a ser guardado.
+	 * @return La cantidad de registros guardados.
+	 * @throws AddressNotSaveException Si los datos no son válidos o no se puede guardar la dirección.
+	 */
 	@Override
 	public Integer save(AddressRequestDto addressRequestDto) {
 
