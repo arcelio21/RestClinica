4c1b2326e2bbad18d596c931a2e6a25874a9f784
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-05-27 18:11:47 -0500

    Se agrego documentacion al servicio de provincia

--- src/main/java/com/example/service/address/ServiceProvinceImpl.java
+++ src/main/java/com/example/service/address/ServiceProvinceImpl.java
@@ -1,22 +1,22 @@
 package com.example.service.address;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import com.example.dto.address.province.ProvinceDto;
+import com.example.dtomapper.address.ProvinceMapper;
+import com.example.entity.address.Tprovince;
+import com.example.exception.NoDataFoundException;
+import com.example.exception.address.province.ProvinceNotSaveException;
+import com.example.exception.address.province.ProvinceNotUpdateException;
+import com.example.mapper.address.MapperProvince;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import com.example.dto.address.province.ProvinceDto;
-import com.example.dtomapper.address.ProvinceMapper;
-import com.example.exception.NoDataFoundException;
-import com.example.exception.address.province.ProvinceNotSaveException;
-import com.example.exception.address.province.ProvinceNotUpdateException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import com.example.entity.address.Tprovince;
-import com.example.mapper.address.MapperProvince;
-
+/**
+ * Implementación de la interfaz IServiceProvince que proporciona métodos para operaciones relacionadas con provincias.
+ */
 @RequiredArgsConstructor
 @Service
 public class ServiceProvinceImpl implements IServiceProvince<ProvinceDto, Integer> {
@@ -24,8 +24,14 @@ public class ServiceProvinceImpl implements IServiceProvince<ProvinceDto, Intege
 
 	private final MapperProvince mapperProvince;
 	private final ProvinceMapper provinceMapper;
-	
 
+
+	/**
+	 * Obtiene una lista de todas las provincias disponibles.
+	 *
+	 * @return Una lista de objetos ProvinceDto que representan las provincias disponibles.
+	 * @throws NoDataFoundException Si no se encuentran datos de provincias disponibles.
+	 */
 	@Override
 	public List<ProvinceDto> getAll() {
 
@@ -43,6 +49,13 @@ public class ServiceProvinceImpl implements IServiceProvince<ProvinceDto, Intege
 	}
 
 
+	/**
+	 * Obtiene un objeto ProvinceDto por su identificador.
+	 *
+	 * @param id El identificador de la provincia.
+	 * @return Un objeto ProvinceDto que representa la provincia encontrada.
+	 * @throws NoDataFoundException Si no se encuentra ninguna provincia para el identificador especificado.
+	 */
 	@Override
 	public ProvinceDto getById(Integer id) {
 		if(id==null || id==0) {
@@ -54,6 +67,13 @@ public class ServiceProvinceImpl implements IServiceProvince<ProvinceDto, Intege
 				.orElseThrow(()-> new NoDataFoundException(id));
 	}
 
+	/**
+	 * Actualiza una provincia existente con la información proporcionada en el objeto ProvinceDto.
+	 *
+	 * @param provinceDto El objeto ProvinceDto con la información de la provincia a actualizar.
+	 * @return El identificador de la provincia actualizada.
+	 * @throws ProvinceNotUpdateException Si los datos de la provincia no son válidos para la actualización.
+	 */
 	@Override
 	public Integer update(ProvinceDto provinceDto) {
 		
@@ -70,6 +90,13 @@ public class ServiceProvinceImpl implements IServiceProvince<ProvinceDto, Intege
 				.orElseThrow(()-> new ProvinceNotUpdateException("Fallo de actualizacion de provincia",provinceDto));
 	}
 
+	/**
+	 * Guarda un objeto ProvinceDto en el sistema.
+	 *
+	 * @param provinceDto El objeto ProvinceDto a ser guardado.
+	 * @return La cantidad de registros guardados.
+	 * @throws ProvinceNotSaveException Si los datos no son válidos o no se puede guardar la provincia.
+	 */
 	@Override
 	public Integer save(ProvinceDto provinceDto) {
 
