02122b27adcbb3915c7e51f0f9ae9d647230de64
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-06-05 23:52:44 -0500

    Se cambio el orden de los metodos, y se dividio en, metodos de obtencion de datos, de persistencia y validacion de campos

--- src/main/java/com/example/service/user/ServiceUserRegImpl.java
+++ src/main/java/com/example/service/user/ServiceUserRegImpl.java
@@ -8,6 +8,7 @@ import com.example.dto.user.user_reg.UserUpdatePassDto;
 import com.example.dtomapper.address.DtoAddressMappper;
 import com.example.dtomapper.user.DtoUserRegMapper;
 import com.example.entity.address.Taddress;
+import com.example.entity.user.TuserReg;
 import com.example.exception.NoDataFoundException;
 import com.example.exception.address.AddressNotSaveException;
 import com.example.exception.address.AddressNotUpdateException;
@@ -27,14 +28,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class ServiceUserRegImpl implements IServiceUserReg{
+public class ServiceUserRegImpl implements IServiceUserReg<UserRegDto, Long, UserRegUpdateDto, UserRegSaveDto>{
 
 	private final MapperUserReg mapperUserReg;
 	private final DtoUserRegMapper dtoUserRegMapper;
@@ -47,126 +49,60 @@ public class ServiceUserRegImpl implements IServiceUserReg{
 	private final PasswordEncoder passwordEncoder;
 
 
-	//TODO CAMBIAR IMPLEMENTACION, DE ESTA FORMA NO DEVUELVE EXCEPCION
+	/*-------------------------METODO DE OBTENCION DE DATOS ----------------*/
 	@Override
 	public List<UserRegDto> getAll() {
 
-		return Optional.ofNullable(this.mapperUserReg.getAll())
-				.orElseThrow(NoDataFoundException::new)
-				.stream()
-				.map(this.dtoUserRegMapper::TuserRegToUserRegDto)
-				.toList();
+		Optional<List<TuserReg>> optionalTuserRegs = Optional.ofNullable(this.mapperUserReg.getAll());
+
+		if(optionalTuserRegs.isPresent() && !optionalTuserRegs.get().isEmpty()){
+
+			return optionalTuserRegs
+					.get()
+					.stream()
+					.map(this.dtoUserRegMapper::TuserRegToUserRegDto)
+					.collect(Collectors.toList());
+		}
+
+		throw new NoDataFoundException();
 	}
 
 	@Override
 	public UserRegDto getById(Long id) {
-		return Optional.ofNullable(id)
+
+		if(id==null || id<=0){
+			throw new NoDataFoundException(id);
+
+		}
+
+		return Optional.of(id)
 				.map(this.mapperUserReg::getById)
 				.map(this.dtoUserRegMapper::TuserRegToUserRegDto)
 				.orElseThrow(() -> new NoDataFoundException(id));
 	}
 
-	@Transactional
-	@Override
-	public Integer update(UserRegDto userRegDto) {
-
-		if(userRegDto==null || userRegDto.getId()==null) {
-			throw  new UserNotUpdateException("Datos no validos ");
-		}
-
-
-		if(!userRegDto.getClass().equals(UserRegUpdateDto.class)) throw new UserNotUpdateException("Datos no validos");
-
-		Taddress address = this.dtoAddressMappper.userRegUpdateDtoToTaddres((UserRegUpdateDto) userRegDto);
-
-		try {
-			Optional.ofNullable(address)
-					.map(this.mapperAddress::update)
-					.orElseThrow(()-> new AddressNotUpdateException("Datos de direccion no son validos"));
-		}catch (Exception e){
-			log.info(e.getMessage());
-			throw new AddressNotUpdateException("Datos de direccion no validos", userRegDto);
-		}
-
-		try {
-
-			return Optional.of(userRegDto)
-					.map(this.dtoUserRegMapper::userRegDtoToTuserReg)
-					.map(tuserReg -> {
-						tuserReg.setAddressId(new Taddress(address.getId()));
-						return tuserReg;
-					})
-					.map(this.mapperUserReg::update)
-					.orElseThrow(()-> new UserNotUpdateException("Datos no validos de usuario"));
-
-		}catch (Exception e){
-			throw new UserNotUpdateException("Datos no validos de usuario",(UserRegUpdateDto) userRegDto);
-		}
-		
-	}
-
-	//TODO HACER TEST SI O SI
-	@Transactional
-	@Override
-	public Integer save(UserRegDto user)  {
-
-		if( user == null || !user.getClass().equals(UserRegSaveDto.class)) throw  new UserNotSaveException("Datos de usuario no guardado", user);
-
-
-		Taddress taddress = this.dtoAddressMappper.userRegSaveDtoToTaddres((UserRegSaveDto) user);
-
-
-		try {
-			Optional.ofNullable(taddress)
-					.map(this.mapperAddress::save)
-					.orElseThrow(()-> {
-						throw  new AddressNotSaveException("Datos de direccion no son validos");
-					});
-		}catch (Exception e){
-			throw new AddressNotSaveException("Datos de direccion no validos", user);
-		}
-
-		try {
-			return Optional.of(user)
-					.map(this.dtoUserRegMapper::userRegDtoToTuserReg)
-					.map((tuserReg -> {tuserReg.setAddressId(new Taddress(taddress.getId()));
-						return tuserReg;
-					}))
-					.map(this.mapperUserReg::save)
-					.orElseThrow(()-> new UserNotSaveException("Datos no son validos para ser guardados"));
-
-		}catch ( Exception e){
-			throw new UserNotSaveException("Datos no son validos, verifiquelos", user);
-		}
-
-
-	}
-
 	@Override
 	public List<UserRegDto> getByName(String name) {
-		
-	  if(name==null || name.trim().equals("")) {
-		  throw new NoDataFoundException();
-	  }
 
-	  return Optional.of(name)
-			  .map(this.mapperUserReg::getByName)
-			  .orElseThrow(NoDataFoundException::new)
-			  .stream()
-			  .map(this.dtoUserRegMapper::TuserRegToUserRegDto)
-			  .toList();
+		if(name==null || name.trim().equals("")) {
+			throw new NoDataFoundException();
+		}
+
+		return Optional.of(name)
+				.map(this.mapperUserReg::getByName)
+				.orElseThrow(NoDataFoundException::new)
+				.stream()
+				.map(this.dtoUserRegMapper::TuserRegToUserRegDto)
+				.collect(Collectors.toList());
 	}
 
+	//TODO CREO QUE AGREGO LOS USUARIOS ANTES DE VALIDAR, ARREGLAR O VERIFICAR ESO
 	@Override
-	public UserRegDto validateAccount(AuthenticationRequest user) {
-		
-		if(user==null || user.getIdenCard()==null || user.getPassword()==null) {
-			return null;
-		}
-		
-		if(user.getPassword().trim().equals("")) {
-			user.setPassword("PASS VACIO");
-			return null;
+	public UserRegDto authenticateUserReg(AuthenticationRequest user) {
+
+		if(user==null || user.getIdenCard()==null || user.getPassword()==null || user.getPassword().trim().equals("")) {
+
+			throw new UsernameInvalid("Datos no validos");
 		}
 
 
@@ -177,33 +113,95 @@ public class ServiceUserRegImpl implements IServiceUserReg{
 				)
 		);
 
-		
+
 		return this.mapperUserReg.getByIdenCard(user.getIdenCard())
 				.map(this.dtoUserRegMapper::TuserRegToUserRegDto)
 				.orElseThrow(() -> new UsernameInvalid("Datos no valido"));
 	}
 
+	/*-------------------------------METODOS DE PERSISTENCIA ---------------------------------------------*/
+
+	@Transactional
+	@Override
+	public Integer update(UserRegUpdateDto user) {
+
+		this.validateNullFieldUpdate(user);
+
+		Integer rowUpdateAddress ;
+
+		rowUpdateAddress = Optional.of(user)
+				.map(this.dtoAddressMappper::userRegUpdateDtoToTaddres)
+				.map(this.mapperAddress::update)
+				.orElseThrow(()-> new AddressNotUpdateException("Datos de direccion no son validos"));
+
+		if (rowUpdateAddress==0) throw  new AddressNotUpdateException("Datos de direccion no son validos");
+
+
+
+		return Optional.of(user)
+				.map(this.dtoUserRegMapper::userRegUpdateDtoToTuserReg)
+				.map(tuserReg -> {
+
+					if(tuserReg.getPassword()!=null && !tuserReg.getPassword().trim().isEmpty()){
+						tuserReg.setPassword(passwordEncoder.encode(tuserReg.getPassword()));
+					}
+					return tuserReg;
+				})
+				.map(this.mapperUserReg::update)
+				.orElseThrow(()-> new UserNotUpdateException("Datos no validos de usuario"));
+
+	}
+
+	/**
+	 * ESTE METODO APLICARA PARA CUALQUIER USUARIO QUE SE VAYA A GUARDAR USUARIOS, EN CASO DE QUE EL USUARIO SEA UN CLIENTE,
+	 * SE LE DARA LA OPCION DE ENVIO DE LINK PARA CONTRASEÑA Y UNA PREGUNTA PARA VALIDAR IDENTIDAD
+	 * @param user
+	 * @return
+	 */
+	//TODO HACER TEST SI O SI | SEPARAR CODIGO DE VALIDACION EN UN METODO
+	@Transactional
+	@Override
+	public Integer save(UserRegSaveDto user)  {
+
+		this.validateNullFieldSave(user);
+
+		Long idAddress;
+
+		idAddress=Optional.of(user)
+				.map(this.dtoAddressMappper::userRegSaveDtoToTaddres)
+				.map(this.mapperAddress::save)
+				.orElseThrow(()-> new AddressNotSaveException("Datos de direccion no son validos"));
+
+		if(idAddress==null || idAddress==0) throw new AddressNotSaveException("Datos de direccion no son validos");
+
+
+		return Optional.of(user)
+				.map(this.dtoUserRegMapper::userRegSaveDtoToTuserReg)
+				.map(tuserReg -> {
+					tuserReg.setAddressId(new Taddress(idAddress));
+
+					if(tuserReg.getPassword()!=null && !tuserReg.getPassword().trim().isEmpty()){
+						tuserReg.setPassword(passwordEncoder.encode(tuserReg.getPassword()));
+					}
+					return tuserReg;
+				})
+				.map(this.mapperUserReg::save)
+				.orElseThrow(()-> new UserNotSaveException("Datos no son validos para ser guardados"));
+
+
+	}
+
+
+	//TODO VERIFICAR SI ES NECESARIO LA CONTRASEÑA OLD, EN CASO DE QUE NO SE HAYA AGREGADO AL USUARIO
 	@Override
 	public Integer updatePassword( UserUpdatePassDto user) {
 		
-		if(user==null ){
-			throw new PasswordNotUpdateException("Datos no enviados");
+		if(user==null || user.getNewPassword()==null || user.getNewPassword().trim().isEmpty()
+			|| user.getIndeCard()==null || user.getIndeCard()<=0
+			){
+			throw new PasswordNotUpdateException("Datos no validos");
 		}
 
-		List<String> properties = null;
-
-		try {
-			 properties = Arrays.asList(user.getOldPassword(), user.getIndeCard().toString(), user.getNewPassword());
-		}catch (RuntimeException e){
-			throw new PasswordNotUpdateException("Datos mo validos", user);
-		}
-
-		long count = properties.stream()
-				.filter(p -> p != null && !p.trim().equals(""))
-				.count();
-
-
-		if(count<properties.size()) throw new PasswordNotUpdateException(" Cantidad Datos no son validos", user);
 
 		UserDetails userDetails = userDetailsService.loadUserByUsername(user.getIndeCard().toString());
 
@@ -227,6 +225,39 @@ public class ServiceUserRegImpl implements IServiceUserReg{
 					.map(tuserReg -> this.mapperUserReg.updatePassword(tuserReg,userValid.getNewPassword()))
 					.orElseThrow(()-> new PasswordNotUpdateException("Error al actualizar usuario, datos de usuario no validos", user));
 
+	}
+
+
+	/* -------------------- METODOS DE VALIDACION DE DATOS DE LOS CAMPOS -------------------*/
+	private void validateNullFieldUpdate(UserRegUpdateDto user) throws UserNotUpdateException{
+
+		if(user.getId()==null || user.getId()<=0
+				|| user.getAddressId()==null || user.getAddressId()<=0
+				|| user.getEmail()==null || user.getEmail().trim().isEmpty()
+				|| user.getDirecSpecific()==null || user.getDirecSpecific().trim().isEmpty()
+				|| user.getName()==null || user.getName().trim().isEmpty()
+				|| user.getLastName()==null || user.getLastName().trim().isEmpty()
+				|| user.getIdenCard()==null || user.getIdenCard()<=0
+				|| user.getBirthday()==null
+				|| user.getVillageId()==null || user.getVillageId()<=0
+		) {
+			throw  new UserNotUpdateException("Datos no validos", user);
+		}
+
+	}
+
+	private void validateNullFieldSave(UserRegSaveDto user){
+		if(user.getEmail()==null || user.getEmail().trim().isEmpty()
+				|| user.getDirecSpecific()==null || user.getDirecSpecific().trim().isEmpty()
+				|| user.getName()==null || user.getName().trim().isEmpty()
+				|| user.getLastName()==null || user.getLastName().trim().isEmpty()
+				|| user.getIdenCard()==null || user.getIdenCard()<=0
+				|| user.getBirthday()==null
+				|| user.getVillageId()==null || user.getVillageId()<=0
+		) {
+
+			throw  new UserNotSaveException("Datos no validos");
+		}
 	}
 
 }
