13b2838ac09637d916ea20052cbc1ae09ee022a7
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-06-06 10:54:54 -0500

    Se cambio anotacion de controlador de errores

--- src/main/java/com/example/exception/ControllerAdvisor.java
+++ src/main/java/com/example/exception/ControllerAdvisor.java
@@ -32,8 +32,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -41,7 +41,7 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NoDataFoundException.class)
