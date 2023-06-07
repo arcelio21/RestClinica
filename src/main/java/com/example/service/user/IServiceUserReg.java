eb180bccfc233fd1bb73978639b4b93bc6943e66
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-06-06 20:12:43 -0500

    Se elimino metodo innecesario de interface

--- src/main/java/com/example/service/user/IServiceUserReg.java
+++ src/main/java/com/example/service/user/IServiceUserReg.java
@@ -1,6 +1,5 @@
 package com.example.service.user;
 
-import com.example.dto.AuthenticationRequest;
 import com.example.dto.user.user_reg.UserUpdatePassDto;
 import com.example.service.ServiceTemplateCrud;
 
@@ -9,7 +8,6 @@ import java.util.List;
 public interface IServiceUserReg<GET,ID,UPDATE,SAVE> extends ServiceTemplateCrud<GET,ID,UPDATE,SAVE>{
 
 	List<GET> getByName(String name);
-	GET authenticateUserReg(AuthenticationRequest user);
 	Integer updatePassword(UserUpdatePassDto user);
 	
 }
