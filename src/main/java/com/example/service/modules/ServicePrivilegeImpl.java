daef6b4c1dbbb1e16ff741eb47a13531fcee4fda
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-06-29 12:20:00 -0500

    Se implemento metodo de obtencion de lista completa de privileges

--- src/main/java/com/example/service/modules/ServicePrivilegeImpl.java
+++ src/main/java/com/example/service/modules/ServicePrivilegeImpl.java
@@ -1,21 +1,42 @@
 package com.example.service.modules;
 
+import com.example.dto.modules.privileges.PrivilegeDto;
+import com.example.dtomapper.modules.DtoPrivilegeMapper;
 import com.example.entity.modules.Tprivilege;
+import com.example.exception.NoDataFoundException;
+import com.example.mapper.modules.MapperPrivilege;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 @Service
 public class ServicePrivilegeImpl implements IServicePrivilege{
 
 
+	private final MapperPrivilege mapperPrivilege;
+	private final DtoPrivilegeMapper dtoPrivilegeMapper;
 	@Override
-	public List<Tprivilege> getAll() {
-		return null;
+	public List<PrivilegeDto> getAll() {
+
+		Optional<List<Tprivilege>> optionalTprivileges = Optional.ofNullable(this.mapperPrivilege.getAll());
+
+		if(optionalTprivileges.isPresent() && !optionalTprivileges.get().isEmpty()){
+
+			return optionalTprivileges.get()
+					.stream()
+					.map(this.dtoPrivilegeMapper::TprivilegeToPrivilegDto)
+					.collect(Collectors.toList());
+		}
+
+		throw new NoDataFoundException("Datos no encontrados");
 	}
 
 	@Override
-	public Tprivilege getById(Integer id) {
+	public PrivilegeDto getById(Integer id) {
 		return null;
 	}
 
