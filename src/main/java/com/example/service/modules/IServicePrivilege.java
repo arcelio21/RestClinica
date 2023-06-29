2962f21dfa1bd2b9855d31a37bc8747ac9dd04d5
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-06-29 14:50:19 -0500

    Parametro del tipo de objeto para actualizar privilege agregado

--- src/main/java/com/example/service/modules/IServicePrivilege.java
+++ src/main/java/com/example/service/modules/IServicePrivilege.java
@@ -1,9 +1,10 @@
 package com.example.service.modules;
 
 import com.example.dto.modules.privileges.PrivilegeDto;
+import com.example.dto.modules.privileges.PrivilegeUpdateDto;
 import com.example.entity.modules.Tprivilege;
 import com.example.service.ServiceTemplateCrud;
 
-public interface IServicePrivilege extends ServiceTemplateCrud<PrivilegeDto, Integer,Tprivilege,Tprivilege>{
+public interface IServicePrivilege extends ServiceTemplateCrud<PrivilegeDto, Integer, PrivilegeUpdateDto,Tprivilege>{
 
 }
