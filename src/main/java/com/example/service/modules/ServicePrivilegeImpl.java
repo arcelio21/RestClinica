a60d273bc1cfd1852585cb6c9f74b97e5218193d
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-06-28 23:51:02 -0500

    Implementacion reseteada

--- src/main/java/com/example/service/modules/ServicePrivilegeImpl.java
+++ src/main/java/com/example/service/modules/ServicePrivilegeImpl.java
@@ -1,62 +1,31 @@
 package com.example.service.modules;
 
-import java.util.Collections;
-import java.util.List;
-
 import com.example.entity.modules.Tprivilege;
-import com.example.mapper.modules.MapperPrivilege;
-
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ServicePrivilegeImpl implements IServicePrivilege{
 
-	private MapperPrivilege mapperPrivilege;	
-	
-	public ServicePrivilegeImpl(MapperPrivilege mapperPrivilege) {
-		this.mapperPrivilege = mapperPrivilege;
-	}
 
 	@Override
 	public List<Tprivilege> getAll() {
-
-		List<Tprivilege> privileges=this.mapperPrivilege.getAll();
-		if(privileges==null || privileges.isEmpty()) {
-			return Collections.emptyList();
-		}
-		return privileges;
+		return null;
 	}
 
 	@Override
 	public Tprivilege getById(Integer id) {
-
-		if(id==null || id<=0) {
-			return null;
-		}
-		
-		return this.mapperPrivilege.getByid(id);
+		return null;
 	}
 
 	@Override
 	public Integer update(Tprivilege tprivilege) {
-
-		if(tprivilege==null || tprivilege.getId()==null || tprivilege.getId()<=0) {
-			return 0; 
-		}
-		
-		return this.mapperPrivilege.update(tprivilege);
+		return null;
 	}
 
 	@Override
 	public Integer save(Tprivilege tprivilege){
-	    if(tprivilege==null){
-	    	return 0;
-	    }
-	    
-	    if(tprivilege.getId()!=null || tprivilege.getId()>=0) {
-	    	return 0;
-	    }
-
-	    return this.mapperPrivilege.save(tprivilege);
+	    return null;
 	}
 }
