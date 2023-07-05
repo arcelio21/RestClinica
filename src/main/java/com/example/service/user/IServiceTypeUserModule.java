60f638739b9a9166faa6e91ff394d0e5e5cece98
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-05 14:58:22 -0500

    Actualizacion de tipos de datos para manipular datos de privilegios de modulos asociado a un tipo de usuario

--- src/main/java/com/example/service/user/IServiceTypeUserModule.java
+++ src/main/java/com/example/service/user/IServiceTypeUserModule.java
@@ -1,12 +1,14 @@
 package com.example.service.user;
+import com.example.dto.user.typeuser_module.TypeUserModuleGetDto;
+import com.example.dto.user.typeuser_module.TypeUserModuleSaveDto;
+import com.example.dto.user.typeuser_module.TypeUserModuleUpdateDto;
 import com.example.service.ServiceTemplateCrud;
 
 import java.util.List;
 
 import com.example.entity.user.TtypeUserModule;
 
-public interface IServiceTypeUserModule extends ServiceTemplateCrud<TtypeUserModule,Integer,TtypeUserModule,TtypeUserModule>{
+public interface IServiceTypeUserModule extends ServiceTemplateCrud<TypeUserModuleGetDto, TypeUserModuleGetDto, TypeUserModuleUpdateDto, TypeUserModuleSaveDto>{
     List<TtypeUserModule> getPrivTypeUser(Integer typeUserId);
-    List<TtypeUserModule> getModulePriv(Integer modulePrivilegeId);
-    Integer update (TtypeUserModule idTypUserModule,Integer modulPrivBef);
+    List<TtypeUserModule> getModulePriv(Long modulePrivilegeId);
 }
