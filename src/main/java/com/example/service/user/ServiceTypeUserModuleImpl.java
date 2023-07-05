ce3698196a5ac1de5303888c06b7175a6233a62b
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-05 14:58:54 -0500

    Se reseteo implementacion de privilegios de modulos asociado a un tipo de usuario

--- src/main/java/com/example/service/user/ServiceTypeUserModuleImpl.java
+++ src/main/java/com/example/service/user/ServiceTypeUserModuleImpl.java
@@ -1,84 +1,49 @@
 package com.example.service.user;
 
-import java.util.Collections;
-import java.util.List;
-
+import com.example.dto.user.typeuser_module.TypeUserModuleGetDto;
+import com.example.dto.user.typeuser_module.TypeUserModuleSaveDto;
+import com.example.dto.user.typeuser_module.TypeUserModuleUpdateDto;
 import com.example.entity.user.TtypeUserModule;
 import com.example.mapper.user.MapperTypeUserModule;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class ServiceTypeUserModuleImpl implements IServiceTypeUserModule {
 
-	private MapperTypeUserModule mapperTypeUserModule;
+	private final MapperTypeUserModule mapperTypeUserModule;
 
-	public ServiceTypeUserModuleImpl(MapperTypeUserModule mapperTypeUserModule) {
-		this.mapperTypeUserModule = mapperTypeUserModule;
-	}
 
 	@Override
-	public List<TtypeUserModule> getAll() {
-		List<TtypeUserModule> ttypeUserModules = this.mapperTypeUserModule.getAll();
-		if (ttypeUserModules == null || ttypeUserModules.isEmpty()) {
-			return Collections.emptyList();
-		}
-
-		return ttypeUserModules;
-	}
-
-	@Override
-	@Deprecated
-	public TtypeUserModule getById(Integer id) {
-		if (id == null || id <= 0) {
-		    return null;
-		}
+	public List<TypeUserModuleGetDto> getAll() {
 		return null;
 	}
 
 	@Override
-	@Deprecated
-	public Integer update(TtypeUserModule ttypeUserModule) {
-		// if(ttypeUserModule==null || ttypeUserModule.getModulePrivilegeId()){
-		    
-		// }
-		// return this.mapperTypeUserModule.update(ttypeUserModule, modulPrivBef);
-	      return null;
+	public TypeUserModuleGetDto getById(TypeUserModuleGetDto typeUserModuleGetDto) {
+		return null;
 	}
 
 	@Override
-	public Integer save(TtypeUserModule ttypeUserModule) {
-		if(ttypeUserModule==null){
-			return 0;
-		}
-		return this.mapperTypeUserModule.save(ttypeUserModule);
+	public Integer update(TypeUserModuleUpdateDto t) {
+		return null;
 	}
 
-	
+	@Override
+	public Integer save(TypeUserModuleSaveDto t) {
+		return null;
+	}
 
 	@Override
 	public List<TtypeUserModule> getPrivTypeUser(Integer typeUserId) {
-		if(typeUserId==null || typeUserId<=0){
-		  return Collections.emptyList();
-		}
-		return this.mapperTypeUserModule.getPrivTypeUser(typeUserId);
+		return null;
 	}
 
 	@Override
-	public List<TtypeUserModule> getModulePriv(Integer modulePrivilegeId) {
-		if(modulePrivilegeId==null || modulePrivilegeId<=0){
-		  return Collections.emptyList();
-		}
-		return this.mapperTypeUserModule.getModulePriv(modulePrivilegeId);
+	public List<TtypeUserModule> getModulePriv(Long modulePrivilegeId) {
+		return null;
 	}
-
-	@Override
-	public Integer update(TtypeUserModule ttypeUserModule, Integer modulPrivBef) {
-		if(ttypeUserModule==null || ttypeUserModule.getTypeUser()==null || ttypeUserModule.getTypeUser().getId()==null || ttypeUserModule.getTypeUser().getId()<=0 || modulPrivBef==null || modulPrivBef<=0 ){
-		  return 0;
-		}
-
-		return this.mapperTypeUserModule.update(ttypeUserModule, modulPrivBef);
-	}
-
 }
