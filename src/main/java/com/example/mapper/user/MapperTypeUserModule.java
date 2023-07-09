10c0bc6ffa6c170086e26e7ff211dd5d4b6d196c
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-09 18:53:32 -0500

    Obtencion de todos los datos los privilegios de un modulos asignado a un tipo de usuario

--- src/main/java/com/example/mapper/user/MapperTypeUserModule.java
+++ src/main/java/com/example/mapper/user/MapperTypeUserModule.java
@@ -1,10 +1,35 @@
 package com.example.mapper.user;
 
+import com.example.entity.user.TtypeUserModule;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface MapperTypeUserModule {
 
-
+	/**
+	 * Recupera una lista de TtypeUserModule que contiene información sobre los módulos, privilegios y estados asignados a cada tipo de usuario.
+	 *
+	 * @return Lista de TtypeUserModule con la información solicitada.
+	 */
+	@Select("SELECT Ttm.id, Tm.name_modules AS nameModule, Tu.name_type_user AS nameTypeUser, tpr.name_privilege AS namePrivilege " +
+			"FROM Ttypeusers_modules Ttm " +
+			"INNER JOIN Tmodules_privileges Tp on Ttm.modls_privgs_id = Tp.id " +
+			"INNER JOIN Tmodules Tm on Tp.module_id = Tm.id " +
+			"INNER JOIN Tprivileges tpr ON Tp.privilege_id = tpr.id " +
+			"INNER JOIN Ttypes_users Tu on Ttm.type_user_id = Tu.id")
+	@Results(id = "typeUserModuleMap",
+			value = {
+					@Result(column = "id", property = "id"),
+					@Result(column = "nameModule",property = "modulePrivilegeId.module.nameModule"),
+					@Result(column = "nameTypeUser",property = "typeUser.nameTypeUser"),
+					@Result(column = "namePrivilege", property = "modulePrivilegeId.privilege.namePrivilege")
+			}
+	)
+	List<TtypeUserModule> getAll();
 	
 }
