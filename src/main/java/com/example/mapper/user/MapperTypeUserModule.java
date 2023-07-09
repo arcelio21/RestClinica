792482dea330f8e7affb1858e02721d2e77558c1
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-09 18:55:54 -0500

    Obtencion de todos los datos por ID de  los privilegios de un modulos asignado a un tipo de usuario

--- src/main/java/com/example/mapper/user/MapperTypeUserModule.java
+++ src/main/java/com/example/mapper/user/MapperTypeUserModule.java
@@ -2,9 +2,12 @@ package com.example.mapper.user;
 
 import com.example.entity.user.TtypeUserModule;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Param;
+
 
 import java.util.List;
 
@@ -31,5 +34,21 @@ public interface MapperTypeUserModule {
 			}
 	)
 	List<TtypeUserModule> getAll();
+
+	/**
+	 * Recupera una lista de TtypeUserModule que contiene información sobre los módulos, privilegios y estados asignados a cada tipo de usuario.
+	 * Filtrado Por id
+	 * @param id
+	 * @return Lista de TtypeUserModule con la información solicitada.
+	 */
+	@Select("SELECT Ttm.id, Tm.name_modules AS nameModule, Tu.name_type_user AS nameTypeUser, tpr.name_privilege AS namePrivilege " +
+			"FROM Ttypeusers_modules Ttm " +
+			"INNER JOIN Tmodules_privileges Tp on Ttm.modls_privgs_id = Tp.id " +
+			"INNER JOIN Tmodules Tm on Tp.module_id = Tm.id " +
+			"INNER JOIN Tprivileges tpr ON Tp.privilege_id = tpr.id " +
+			"INNER JOIN Ttypes_users Tu on Ttm.type_user_id = Tu.id " +
+			"WHERE Ttm.id = #{id}")
+	@ResultMap(value = "typeUserModuleMap")
+	TtypeUserModule getById(@Param("id") Long id);
 	
 }
