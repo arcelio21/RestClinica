5fbcba779aa66934834d26bef5826a426a74431c
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-16 21:57:15 -0500

    Cambio de estructura

--- src/main/java/com/example/dto/user/typeuser_module/TypeUserModuleUpdateDto.java
+++ src/main/java/com/example/dto/user/typeuser_module/TypeUserModuleUpdateDto.java
@@ -12,13 +12,20 @@ import javax.validation.constraints.NotNull;
 @Schema(name = "Type User Module Update Data", description = "Estructura de informacion para guardar datos")
 public class TypeUserModuleUpdateDto {
 
+
     @NotNull
     @Min(1)
-    @Schema(description = "ID  de modulo asociado a un privilegio")
+    @Schema(description = "ID de registro")
+    private Long id;
+
+    @NotNull
+    @Min(1)
+    @Schema(description = "ID de modulo nuevo asociado a un privilegio")
     private Long idModulePrivilege;
 
     @NotNull
     @Min(1)
-    @Schema(description = "ID de tipo de usuario")
-    private Integer typeUser;
+    @Schema(description = "ID de tipo de usuario nuevo")
+    private Integer idTypeUser;
+
 }
