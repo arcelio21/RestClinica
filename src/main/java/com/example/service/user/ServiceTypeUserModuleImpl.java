3cf1ee6b1a1920ed7e572ba8a6c43c416d79d7ac
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-07-19 15:19:15 -0500

    Implementacion de listado de todos los registros de privilegios de modulos asignados a tipo de usuario

--- src/main/java/com/example/service/user/ServiceTypeUserModuleImpl.java
+++ src/main/java/com/example/service/user/ServiceTypeUserModuleImpl.java
@@ -1,29 +1,47 @@
 package com.example.service.user;
 
-import com.example.dto.user.typeuser_module.TypeUserModuleGetDto;
-import com.example.dto.user.typeuser_module.TypeUserModuleSaveDto;
-import com.example.dto.user.typeuser_module.TypeUserModuleUpdateDto;
-import com.example.entity.user.TtypeUserModule;
+import com.example.dto.user.typeuser_module.*;
+import com.example.dtomapper.user.DtoTypeUserModuleMapper;
+import com.example.exception.NoDataFoundException;
 import com.example.mapper.user.MapperTypeUserModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
-public class ServiceTypeUserModuleImpl implements IServiceTypeUserModule {
+public class ServiceTypeUserModuleImpl implements IServiceTypeUserModule<TypeUserModuleGetDto, Long, TypeUserModuleUpdateDto, TypeUserModuleSaveDto> {
 
 	private final MapperTypeUserModule mapperTypeUserModule;
+	private final DtoTypeUserModuleMapper dtoTypeUserModuleMapper;
 
 
+	/**
+	 * Obtiene todos los objetos TypeUserModuleGetDto.
+	 *
+	 * @return Lista de objetos TypeUserModuleGetDto
+	 * @throws NoDataFoundException si no se encuentran datos o la lista está vacía
+	 */
 	@Override
 	public List<TypeUserModuleGetDto> getAll() {
-		return null;
+
+		List<TypeUserModuleGetDto> typeusermodules = Optional.of(this.mapperTypeUserModule.getAll())
+				.orElseThrow(NoDataFoundException::new)
+				.stream()
+				.map(this.dtoTypeUserModuleMapper::tTypeUserModuleToTypeUserModuleGetDto)
+				.toList();
+
+		if(typeusermodules.isEmpty()){
+			throw new NoDataFoundException();
+		}
+
+		return typeusermodules;
 	}
 
 	@Override
-	public TypeUserModuleGetDto getById(TypeUserModuleGetDto typeUserModuleGetDto) {
+	public TypeUserModuleGetDto getById(Long aLong) {
 		return null;
 	}
 
@@ -38,12 +56,37 @@ public class ServiceTypeUserModuleImpl implements IServiceTypeUserModule {
 	}
 
 	@Override
-	public List<TtypeUserModule> getPrivTypeUser(Integer typeUserId) {
+	public List<ModuleTypeUserGetDto> getModuleAndTypeUserDistinct() {
 		return null;
 	}
 
 	@Override
-	public List<TtypeUserModule> getModulePriv(Long modulePrivilegeId) {
+	public List<ModuleOfTypeUserGetDto> getModuleDistinctByIdTypeUserAndStatusActived(Integer idTypeUser) {
+		return null;
+	}
+
+	@Override
+	public List<ModuleOfTypeUserGetDto> getModuleDistinctByIdTypeUserAndIdStatus(Integer idTypeUser, Integer idStatus) {
+		return null;
+	}
+
+	@Override
+	public List<TypeUserOfModuleGetDto> getTypeUserDistinctByIdModuleAndStatusActivated(Long idModule) {
+		return null;
+	}
+
+	@Override
+	public List<TypeUserOfModuleGetDto> getTypeUserDistinctByIdModuleAndIdStatus(Long idModule, Integer idStatus) {
+		return null;
+	}
+
+	@Override
+	public List<PrivilegeOfModuleGetDto> getPrivelegeOfModuleByIdTypeUserAndIdModuleAndStatusActived(Integer idTypeUser, Long idModule) {
+		return null;
+	}
+
+	@Override
+	public List<ModuleTypeUserGetDto> getTypeModulePrivilegeByidTypeUserAndStatusActived(Integer idTypeUser) {
 		return null;
 	}
 }
