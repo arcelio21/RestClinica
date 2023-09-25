c5b86c92d19ffd7617931e48b5a84f0492f5e5c5
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-09-24 19:06:33 -0500

    chore(dto-module): Modificacion en documentacion

--- src/main/java/com/example/dto/user/typeuser_module/TypeUserModuleUpdateDto.java
+++ src/main/java/com/example/dto/user/typeuser_module/TypeUserModuleUpdateDto.java
@@ -9,23 +9,23 @@ import javax.validation.constraints.NotNull;
 
 @Builder
 @Getter
-@Schema(name = "Type User Module Update Data", description = "Estructura de informacion para guardar datos")
+@Schema(name = "Type User Module Update Data", description = "Estructura de informacion para actualizar datos")
 public class TypeUserModuleUpdateDto {
 
 
     @NotNull
     @Min(1)
-    @Schema(description = "ID de registro")
+    @Schema(description = "ID de registro", name = "id")
     private Long id;
 
     @NotNull
     @Min(1)
-    @Schema(description = "ID de modulo nuevo asociado a un privilegio")
+    @Schema(name = "idModulePrivilege",description = "ID de modulo nuevo asociado a un privilegio")
     private Long idModulePrivilege;
 
     @NotNull
     @Min(1)
-    @Schema(description = "ID de tipo de usuario nuevo")
+    @Schema(name = "idTypeUser",description = "ID de tipo de usuario nuevo")
     private Integer idTypeUser;
 
 }
