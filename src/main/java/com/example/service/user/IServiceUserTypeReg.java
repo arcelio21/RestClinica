80c2168dae65a950cf4379d92010b185a4beecd7
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-10-02 17:02:53 -0500

    dosc(user): Documentacion de interface de tipos de usuario para usuario

--- src/main/java/com/example/service/user/IServiceUserTypeReg.java
+++ src/main/java/com/example/service/user/IServiceUserTypeReg.java
@@ -1,20 +1,28 @@
- package com.example.service.user;
+package com.example.service.user;
 
 import java.util.List;
 
 import com.example.service.ServiceTemplateCrud;
 
-public interface IServiceUserTypeReg<GET,ID,UPDATE,SAVE> extends ServiceTemplateCrud<GET,ID,UPDATE,SAVE>{
+/**
+ * Interfaz genérica para definir un servicio relacionado con la entidad de
+ * usuario tipo-registro.
+ *
+ * @param <GET>    El tipo de objeto que se utiliza para obtener datos de
+ *                 usuario tipo-registro.
+ * @param <ID>     El tipo de dato que se utiliza para identificar registros de
+ *                 usuario tipo-registro.
+ * @param <UPDATE> El tipo de objeto que se utiliza para actualizar registros de
+ *                 usuario tipo-registro.
+ * @param <SAVE>   El tipo de objeto que se utiliza para guardar nuevos
+ *                 registros de usuario tipo-registro.
+ */
+public interface IServiceUserTypeReg<GET, ID, UPDATE, SAVE> extends ServiceTemplateCrud<GET, ID, UPDATE, SAVE> {
 
- 
+	List<GET> getByIdUserReg(ID id);
 
-	 List<GET> getByIdUserReg(ID id);
+	List<GET> getByIdTypeUser(ID id);
 
-	List<GET> getByIdTypeUser( ID id);
-
-	 List<GET> getByIdStatus(ID id);
+	List<GET> getByIdStatus(ID id);
 
 }
-
-
- 
