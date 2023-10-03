96dc77776ce51138ddc8bf9862a4ce1cd9975a0b
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-10-02 21:52:01 -0500

    feat(user-mapperdto): Agrega conversor para userRegOfTypeUser

--- src/main/java/com/example/dtomapper/user/DtoUserTypeRegMapper.java
+++ src/main/java/com/example/dtomapper/user/DtoUserTypeRegMapper.java
@@ -1,29 +1,20 @@
 package com.example.dtomapper.user;
 
-import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 import com.example.dto.user.type_user_reg.TypeUserOfUserRegGetDto;
+import com.example.dto.user.type_user_reg.UserRegOfTypeUserGetDto;
 import com.example.dto.user.type_user_reg.UserTypeRegGetDto;
 import com.example.entity.user.TuserTypeReg;
 
-@Mapper
+@Mapper(uses = DtoUserTypeRegDecoratorMapperImpl.class)
 public interface DtoUserTypeRegMapper {
     
     DtoUserTypeRegMapper INSTANCE = Mappers.getMapper(DtoUserTypeRegMapper.class);
 
-    @BeforeMapping
-    default void validateFullName(TuserTypeReg user){
-        if(user==null 
-            || user.getUserRegId().getName() == null || user.getUserRegId().getName().isEmpty()
-            || user.getUserRegId().getLastName() == null || user.getUserRegId().getLastName().isEmpty()){
-
-            throw new RuntimeException("ERROR DE MAPEO DE FULLNAME");
-        }
-    }
 
     @Mappings(value = {
         @Mapping(target = "id", source = "user.id"),
@@ -40,4 +31,12 @@ public interface DtoUserTypeRegMapper {
         @Mapping(target = "status", source = "user.statusId.name")
     })
     TypeUserOfUserRegGetDto tuserTypeRegToTypeUserOfUserRegGet(TuserTypeReg user);
+
+    @Mappings(value = {
+        @Mapping(target = "id", source = "user.id"),
+        @Mapping(target = "fullName", expression = "java(user.getUserRegId().getName() + ' '+ user.getUserRegId().getLastName())"),
+        @Mapping(target = "idenCard", source = "user.userRegId.idenCard"),
+        @Mapping(target = "status", source = "user.statusId.name")
+    })
+    UserRegOfTypeUserGetDto tuserTypeRegToUserRegOfTypeUserGet(TuserTypeReg user);
 }
