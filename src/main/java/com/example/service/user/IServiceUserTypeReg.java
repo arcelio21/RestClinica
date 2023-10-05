862de35e1ec05f651ca5024819642e7905eaafb3
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-10-05 14:50:06 -0500

    docs(user-service): Elimina signos innecesario

--- src/main/java/com/example/service/user/IServiceUserTypeReg.java
+++ src/main/java/com/example/service/user/IServiceUserTypeReg.java
@@ -10,13 +10,13 @@ import com.example.service.ServiceTemplateCrud;
  * Interfaz genérica para definir un servicio relacionado con la entidad de
  * usuario tipo-registro.
  *
- * @param <GET>    El tipo de objeto que se utiliza para obtener datos de
+ * @param GET   El tipo de objeto que se utiliza para obtener datos de
  *                 usuario tipo-registro.
- * @param <ID>     El tipo de dato que se utiliza para identificar registros de
+ * @param ID     El tipo de dato que se utiliza para identificar registros de
  *                 usuario tipo-registro.
- * @param <UPDATE> El tipo de objeto que se utiliza para actualizar registros de
+ * @param UPDATE El tipo de objeto que se utiliza para actualizar registros de
  *                 usuario tipo-registro.
- * @param <SAVE>   El tipo de objeto que se utiliza para guardar nuevos
+ * @param SAVE   El tipo de objeto que se utiliza para guardar nuevos
  *                 registros de usuario tipo-registro.
  */
 public interface IServiceUserTypeReg<GET, ID, UPDATE, SAVE> extends ServiceTemplateCrud<GET, ID, UPDATE, SAVE> {
