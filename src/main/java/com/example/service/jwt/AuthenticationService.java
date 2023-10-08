0be7e0c2b49958d28679a2551c5ffabf38a69de7
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-10-08 14:07:51 -0500

    feat(auth-service): Modifica pasos para registrar usuario
     -Agrega la posibilidad de guardar roles que se asocien a un usuario.
     -Refactoriza el metodo que registra usuario
    
    refs: 29ffcfb

--- src/main/java/com/example/service/jwt/AuthenticationService.java
+++ src/main/java/com/example/service/jwt/AuthenticationService.java
@@ -1,16 +1,28 @@
 package com.example.service.jwt;
 
+import com.example.exception.NoDataFoundException;
+import com.example.exception.address.AddressNotSaveException;
+import com.example.exception.user.type_user_reg.UserTypeRegNotSaveException;
+import com.example.exception.user.user_reg.UserNotSaveException;
 import com.example.exception.user.user_reg.UsernameInvalid;
 import com.example.security.JwtUtils;
 import com.example.dto.AuthenticationRequest;
 import com.example.dto.AuthenticationResponse;
 import com.example.dto.RegisterRequest;
+import com.example.dto.user.type_user.TypeUserSaveDto;
+import com.example.dto.user.type_user_reg.UserTypeRegSaveDto;
+import com.example.dtomapper.user.DtoUserTypeRegMapper;
 import com.example.entity.address.Taddress;
 import com.example.entity.address.Tvillage;
 import com.example.entity.user.TuserReg;
 import com.example.mapper.address.MapperAddress;
 import com.example.mapper.user.MapperUserReg;
+import com.example.mapper.user.MapperUserTypeReg;
+
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,6 +35,9 @@ public class AuthenticationService {
 
     private final MapperUserReg mapperUserReg;
     private final MapperAddress mapperAddress;
+    private final MapperUserTypeReg mapperUserTypeReg;
+
+    private final DtoUserTypeRegMapper dtoUserTypeRegMapper;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtils jwtUtils;
     private final AuthenticationManager authenticationManager;
@@ -36,35 +51,13 @@ public class AuthenticationService {
     @Transactional
     public AuthenticationResponse register(RegisterRequest request){
 
-        var addressAuth = Taddress.builder()
-                .specificAddress(request.getDirecSpecific())
-                .villageId(new Tvillage(request.getIdVillage()))
-                .build();
-
-        Long rowAffected = this.mapperAddress.save(addressAuth);
-        if(rowAffected==1){
-            var userAuth = new TuserReg();
-            userAuth.setIdenCard(request.getIdenCard());
-            userAuth.setEmail(request.getEmail());
-            userAuth.setName(request.getName());
-            userAuth.setLastName(request.getLastName());
-            userAuth.setContact(request.getContact());
-            userAuth.setBirthday(request.getBirthday());
-            userAuth.setPassword(passwordEncoder.encode(request.getPassword()));
-            userAuth.setAddressId(addressAuth);
-
-            var response = this.mapperUserReg.save(userAuth);
-            if(response!=null && response==1){
-                var jwt = this.jwtUtils.generateToken(userAuth);
-                return AuthenticationResponse.builder()
-                        .token(jwt)
-                        .build();
-            }else {
-                return null;
-            }
-        }else {
-            return  null;
-        }
+        Long idAddress = this.saveAddress(request.getDirecSpecific(), request.getIdVillage());
+        
+        Long idUserReg = this.saveUserReg(request, idAddress);
+        
+        this.saveUserTypeReg(request.getRoles(), idUserReg);
+        
+        return this.generateToken(request.getIdenCard());
     }
 
     /**
@@ -95,5 +88,100 @@ public class AuthenticationService {
                 .token(jwt)
                 .build();
 
+    }
+
+    /**
+     * Guarda una dirección de usuario con la dirección específica y el ID de la localidad proporcionados.
+     *
+     * @param direcSpecific La dirección específica que se va a guardar.
+     * @param idVillage El ID de la localidad a la que se asociará la dirección.
+     * @return El ID de la dirección guardada.
+     * @throws AddressNotSaveException si no se puede guardar la dirección o si los datos de la dirección no son válidos.
+     */
+    private Long saveAddress(String direcSpecific, Integer idVillage){
+        
+        var addressAuth = Taddress.builder()
+                .specificAddress(direcSpecific)
+                .villageId(new Tvillage(idVillage))
+                .build();
+
+        Long rowAffected = this.mapperAddress.save(addressAuth);
+
+        if(rowAffected==null || rowAffected <= 0 || addressAuth.getId()==null || addressAuth.getId()<=0){
+            throw new AddressNotSaveException("Data Address Not Valid");
+        }
+
+        return addressAuth.getId();
+    }
+
+    /**
+     * Guarda un registro de usuario con los datos proporcionados en la solicitud y el ID de la dirección asociada.
+     *
+     * @param request La solicitud que contiene los datos del registro de usuario.
+     * @param idAddress El ID de la dirección asociada al registro de usuario.
+     * @return El ID del registro de usuario guardado.
+     * @throws UserNotSaveException si no se puede guardar el registro de usuario o si los datos del registro no son válidos.
+     */
+    private Long saveUserReg(RegisterRequest request, Long idAddress){
+        
+        var userAuth = new TuserReg();
+        userAuth.setIdenCard(request.getIdenCard());
+        userAuth.setEmail(request.getEmail());
+        userAuth.setName(request.getName());
+        userAuth.setLastName(request.getLastName());
+        userAuth.setContact(request.getContact());
+        userAuth.setBirthday(request.getBirthday());
+        userAuth.setPassword(passwordEncoder.encode(request.getPassword()));
+        userAuth.setAddressId(new Taddress(idAddress));
+
+        var response = this.mapperUserReg.save(userAuth);
+
+        if(response == null || response<=0 || userAuth.getId() == null || userAuth.getId()<=0){
+            throw new UserNotSaveException("Data User Not Valid");
+        }
+
+        return userAuth.getId();
+    }
+
+    /**
+     * Guarda los roles de tipo de usuario asociados a un registro de usuario.
+     *
+     * @param roles La lista de roles de tipo de usuario a guardar.
+     * @param idUserReg El ID del registro de usuario al que se asocian los roles.
+     * @throws UserTypeRegNotSaveException si no se pueden guardar los roles de tipo de usuario o si los datos de los roles no son válidos.
+     */
+    private void saveUserTypeReg(List<TypeUserSaveDto> roles, Long idUserReg){
+
+        if(roles==null ||  roles.isEmpty()){
+            throw new UserTypeRegNotSaveException("Data Role Not Valid");
+        }
+        
+        for( TypeUserSaveDto userType:roles){
+            UserTypeRegSaveDto userTypeRegSaveDto = new UserTypeRegSaveDto(idUserReg, userType.id(), 1);
+            Integer rowAffect = this.mapperUserTypeReg.save(this.dtoUserTypeRegMapper.userTypeRegSaveToTuserTypeReg(userTypeRegSaveDto));
+
+            if(rowAffect == null || rowAffect<=0){
+                throw new UserTypeRegNotSaveException("Data Role Not Valid");
+            }
+        }
+
+    }
+
+    /**
+     * Genera un token de autenticación para un usuario identificado por su número de identificación.
+     *
+     * @param idenCard El número de identificación del usuario para el que se generará el token.
+     * @return Un objeto AuthenticationResponse que contiene el token generado.
+     * @throws NoDataFoundException si el usuario no se encuentra o si los datos no son válidos.
+     */
+    private AuthenticationResponse generateToken(Long idenCard){
+
+        TuserReg userValited = this.mapperUserReg.getByIdenCard(idenCard).orElseThrow(() -> new NoDataFoundException("Data Not Valited"));
+
+        var jwt = this.jwtUtils.generateToken(userValited);
+
+        return AuthenticationResponse.builder()
+                .token(jwt)
+                .build();
     }
 }
