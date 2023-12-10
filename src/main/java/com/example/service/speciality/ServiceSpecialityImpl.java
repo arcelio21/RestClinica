ad53ef1d60403b3dbf98aceb0a14ec4c4bf4b574
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-12-09 21:30:59 -0500

    feat(speciality): Agrega obtencion de todas las especialidades

--- src/main/java/com/example/service/speciality/ServiceSpecialityImpl.java
+++ src/main/java/com/example/service/speciality/ServiceSpecialityImpl.java
@@ -4,11 +4,15 @@ import com.example.dto.speciality.speciality.SpecialityGetDto;
 import com.example.dto.speciality.speciality.SpecialitySaveDto;
 import com.example.dto.speciality.speciality.SpecialityUpdateDto;
 import com.example.dtomapper.speciality.DtoSpecialityMapper;
+import com.example.exception.NoDataFoundException;
 import com.example.mapper.speciality.MapperSpeciality;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class ServiceSpecialityImpl implements IServiceSpeciality<SpecialityGetDto,Integer, SpecialityUpdateDto, SpecialitySaveDto>{
@@ -16,9 +20,28 @@ public class ServiceSpecialityImpl implements IServiceSpeciality<SpecialityGetDt
 
 	private final MapperSpeciality mapperSpeciality;
 	private final DtoSpecialityMapper dtoSpecialityMapper;
+
+	/**
+	 * Obtiene una lista de todas los tipos de especialidades disponibles.
+	 *
+	 * @return Una lista de objetos SpecialityGetDto que representan las especialidades.
+	 * @throws NoDataFoundException si no se encuentran datos de especialidades o si la lista resultante está vacía.
+	 */
+	@Transactional(readOnly = true)
 	@Override
 	public List<SpecialityGetDto> getAll() {
-		return null;
+
+		List<SpecialityGetDto> specialityList = Optional.ofNullable(this.mapperSpeciality.getAll())
+				.orElseThrow(()-> new NoDataFoundException("DATA NOT FOUND"))
+				.stream()
+				.map(this.dtoSpecialityMapper::TspecialityToSpecialityGet)
+				.toList();
+
+		if(specialityList.isEmpty()){
+			throw new NoDataFoundException("DATA IS EMPTY");
+		}
+
+		return specialityList;
 	}
 
 	@Override
