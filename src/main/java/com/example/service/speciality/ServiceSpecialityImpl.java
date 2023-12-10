7f2b0a52d5701e586c3e375c17ef26057649c298
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-12-09 20:17:17 -0500

    chore(speciality): Redifine clase de servicio

--- src/main/java/com/example/service/speciality/ServiceSpecialityImpl.java
+++ src/main/java/com/example/service/speciality/ServiceSpecialityImpl.java
@@ -1,61 +1,38 @@
 package com.example.service.speciality;
 
-import java.util.Collections;
-import java.util.List;
-
-import com.example.entity.speciality.Tspeciality;
+import com.example.dto.speciality.speciality.SpecialityGetDto;
+import com.example.dto.speciality.speciality.SpecialitySaveDto;
+import com.example.dto.speciality.speciality.SpecialityUpdateDto;
+import com.example.dtomapper.speciality.DtoSpecialityMapper;
 import com.example.mapper.speciality.MapperSpeciality;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+@RequiredArgsConstructor
 @Service
-public class ServiceSpecialityImpl implements IServiceSpeciality<Tspeciality,Integer,Tspeciality,Tspeciality>{
+public class ServiceSpecialityImpl implements IServiceSpeciality<SpecialityGetDto,Integer, SpecialityUpdateDto, SpecialitySaveDto>{
 
-	private MapperSpeciality mapperSpeciality;
 
-	public ServiceSpecialityImpl(MapperSpeciality mapperSpeciality) {
-		this.mapperSpeciality = mapperSpeciality;
-	}
-
-	@Transactional(readOnly = true)
+	private final MapperSpeciality mapperSpeciality;
+	private final DtoSpecialityMapper dtoSpecialityMapper;
 	@Override
-	public List<Tspeciality> getAll() {
-
-		List<Tspeciality> sTspecialities = this.mapperSpeciality.getAll();
-		if(sTspecialities==null || sTspecialities.size()<=0){
-			return Collections.emptyList();
-		}
-		return sTspecialities;
+	public List<SpecialityGetDto> getAll() {
+		return null;
 	}
 
 	@Override
-	@Transactional(readOnly = true)
-	public Tspeciality getById(Integer id) {
-
-		if(id==null || id<=0){
-			return null;
-		}
-		return this.mapperSpeciality.getById(id);
+	public SpecialityGetDto getById(Integer integer) {
+		return null;
 	}
 
 	@Override
-	public Integer update(Tspeciality tspeciality) {
-
-		if(tspeciality==null || tspeciality.getId()==null || tspeciality.getId()<=0){
-			return 0;
-		}
-		return this.mapperSpeciality.update(tspeciality);
+	public Integer update(SpecialityUpdateDto t) {
+		return null;
 	}
 
 	@Override
-	public Integer save(Tspeciality tspeciality) {
-
-		if(tspeciality==null || tspeciality.getId()!=null){
-			return 0;
-		}
-		return this.mapperSpeciality.save(tspeciality);
-	} 
-
-
+	public Integer save(SpecialitySaveDto t) {
+		return null;
+	}
 }
