15d8a6cde7a85cb79a0ef842d63fe13a616ca543
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-12-23 21:49:35 -0500

    feat(userspeciality): Define estructura para datos guardado de usuario asociado a especialidad

---
+++ src/main/java/com/example/dto/speciality/userspeciality/UserSpecialitySaveDto.java
@@ -0,0 +1,26 @@
+package com.example.dto.speciality.userspeciality;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Schema(
+        name = "UserSpecialityUpdate",
+        description = "Estructura para guardado de usuario asociado a especialidad"
+)
+public record UserSpecialitySaveDto(
+        @Schema(name = "idSpeciality", description = "ID de especialidad", example = "1")
+        @Min(1)
+        @NotNull
+        Integer idSpeciality,
+        @Schema(name = "idUserTypeReg", description = "ID de usuario asociado a tipo de usuario", example = "1")
+        @Min(1)
+        @NotNull
+        Long idUserTypeReg,
+        @Schema(name = "idStatus", description = "ID de tipo de estado", example = "1")
+        @Min(1)
+        @NotNull
+        Integer idStatus
+) {
+}
