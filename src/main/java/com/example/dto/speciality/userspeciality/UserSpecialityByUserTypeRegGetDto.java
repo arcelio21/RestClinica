7aa0efe49e617824f82d88e8bed46115504c7c86
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-12-23 21:40:22 -0500

    feat(userspeciality): Define estructura de datos para usuario asociado a especialidad filtrado por ID de usuario asociado a un tipo de usuario

---
+++ src/main/java/com/example/dto/speciality/userspeciality/UserSpecialityByUserTypeRegGetDto.java
@@ -0,0 +1,25 @@
+package com.example.dto.speciality.userspeciality;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Schema(
+        name = "UserSpecialityByUserTypeRegGet",
+        description = "Estructura de datos para usuario asociado a especialidad filtrado por ID de usuario asociado a un tipo de usuario"
+)
+public record UserSpecialityByUserTypeRegGetDto(
+        @Schema(name = "id", description = "ID de recurso", example = "1")
+        @Min(1)
+        @NotNull
+        Long id,
+        @Schema(name = "nameSpeciality", description = "Nombre de especialidad asociada a usuario", example = "Odontologo")
+        @NotBlank
+        String nameSpeciality,
+        @Schema(name = "nameStatus",description = "Descripcion de estado de la especialidad asociada", example = "Activated")
+        @NotBlank
+        String nameStatus
+) {
+}
