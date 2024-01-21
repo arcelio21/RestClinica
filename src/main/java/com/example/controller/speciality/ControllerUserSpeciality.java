cd634ccbdc5cfd2512035e22121ae5e74c603b71
Arcelio Montezuma <arceliomonte15@gmail.com>
2024-01-20 21:45:26 -0500

    feat(userspeciality): Define end-point para guardar registro

--- src/main/java/com/example/controller/speciality/ControllerUserSpeciality.java
+++ src/main/java/com/example/controller/speciality/ControllerUserSpeciality.java
@@ -4,8 +4,10 @@ import com.example.controller.ControllerTemplate;
 import com.example.dto.ErrorResponseDto;
 import com.example.dto.ResponseDTO;
 import com.example.dto.speciality.speciality.SpecialityGetDto;
+import com.example.dto.speciality.speciality.SpecialitySaveDto;
 import com.example.dto.speciality.speciality.SpecialityUpdateDto;
 import com.example.dto.speciality.userspeciality.UserSpecialityGetDto;
+import com.example.dto.speciality.userspeciality.UserSpecialitySaveDto;
 import com.example.dto.speciality.userspeciality.UserSpecialityUpdateDto;
 import com.example.service.speciality.ServiceUserSpeciality;
 import io.swagger.v3.oas.annotations.Operation;
@@ -16,6 +18,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -85,6 +88,28 @@ public class ControllerUserSpeciality extends ControllerTemplate{
                         .info("Cantidad de registros actualizados")
                         .data(this.service.update(userSpecialityUpdateDto))
                         .build()
+        );
+    }
+
+    @Operation(
+            summary = "Guardar nueva UserSpeciality",
+            description = "Se guardara una nueva usuario especialidad",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "UserSpeciality creada correctamente",useReturnTypeSchema = true),
+                    @ApiResponse(responseCode = "400", description = "Datos proporcionado no son validos",content = {
+                            @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+                    })
+            }
+    )
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseDTO<Integer>> save (@RequestBody UserSpecialitySaveDto userSpecialitySaveDto){
+
+
+        return new ResponseEntity<>(ResponseDTO.<Integer>builder()
+                .info("Cantidad de registro guardados")
+                .data(this.service.save(userSpecialitySaveDto))
+                .build(),
+                HttpStatus.CREATED
         );
     }
 
